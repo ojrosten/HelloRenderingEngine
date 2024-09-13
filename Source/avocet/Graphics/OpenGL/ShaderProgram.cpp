85d2fb5911f2c3f0effe06f62752a40ae30faf78
Oliver Rosten <oliver.rosten@gmail.com>
2024-09-13 14:10:49 +0100

    Move code into the avocet library and apply polish
    1. Create an enum class shader_species
    
    2. Trade NULL for nullptr
    
    3. Utilize noexcept
    
    4. Make types (in)equality comparable

--- Source/avocet/Graphics/OpenGL/ShaderProgram.cpp
+++ Source/avocet/Graphics/OpenGL/ShaderProgram.cpp
@@ -8,5 +8,33 @@
 #include "avocet/Graphics/OpenGL/ShaderProgram.hpp"
 
 namespace avocet::opengl {
+  namespace {
+    void check_compilation_success(GLuint shaderID, std::string_view shaderType) {
+      check_success(shaderID, shaderType, "compilation", GL_COMPILE_STATUS, glGetShaderiv, glGetShaderInfoLog);
+    }
+  }
 
+  void check_linking_success(GLuint shaderID) {
+    check_success(shaderID, "program", "linking", GL_LINK_STATUS, glGetProgramiv, glGetProgramInfoLog);
+  }
+
+  [[nodiscard]]
+  std::string to_string(shader_species species) {
+    using enum shader_species;
+    switch(species) {
+    case vertex:   return "vertex";
+    case fragment: return "fragment";
+    }
+
+    throw std::runtime_error{"shader_species: unexpected value"};
+  }
+
+  shader_compiler::shader_compiler(shader_species species, const char* source)
+    : m_Resource{species}
+  {
+    const auto index{m_Resource.handle().index()};
+    glShaderSource(index, 1, &source, nullptr);
+    glCompileShader(index);
+    check_compilation_success(index, to_string(species));
+  }
 }
--- Source/avocet/Graphics/OpenGL/ShaderProgram.hpp
+++ Source/avocet/Graphics/OpenGL/ShaderProgram.hpp
@@ -5,6 +5,65 @@
 //          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
 ////////////////////////////////////////////////////////////////////
 
-namespace avocet::opengl {
+#include "avocet/Graphics/OpenGL/ResourceHandle.hpp"
 
+#include <format>
+#include <string>
+#include <stdexcept>
+
+namespace avocet::opengl {
+  enum class shader_species : GLenum { vertex = GL_VERTEX_SHADER, fragment = GL_FRAGMENT_SHADER };
+
+  [[nodiscard]]
+  std::string to_string(shader_species species);
+
+  class shader_resource {
+    resource_handle m_Handle{};
+  public:
+    explicit shader_resource(shader_species species)
+      : m_Handle{glCreateShader(static_cast<GLenum>(species))}
+    {}
+
+    shader_resource(shader_resource&&) noexcept = default;
+
+    shader_resource& operator=(shader_resource&&) noexcept = default;
+
+    ~shader_resource() { glDeleteShader(m_Handle.index()); }
+
+    [[nodiscard]]
+    friend bool operator==(const shader_resource&, const shader_resource&) noexcept = default;
+
+    [[nodiscard]]
+    const resource_handle& handle() const noexcept { return m_Handle; }
+  };
+
+  class shader_compiler {
+    shader_resource m_Resource;
+  public:
+    shader_compiler(shader_species species, const char* source);
+
+    [[nodiscard]]
+    friend bool operator==(const shader_compiler&, const shader_compiler&) noexcept = default;
+
+    [[nodiscard]]
+    const shader_resource& resource() const noexcept { return m_Resource; }
+  };
+
+  template<class GetStatus>
+  GLint check_status(GLuint shaderID, GLenum status, GetStatus getStatus) {
+    GLint success{};
+    getStatus(shaderID, status, &success);
+    return success;
+  }
+
+  template<class GetStatus, class GetInfoLog>
+  void check_success(GLuint shaderID, std::string_view name, std::string_view buildStage, GLenum status, GetStatus getStatus, GetInfoLog getInfoLog) {
+    if(!check_status(shaderID, status, getStatus)) {
+      GLchar infoLog[512]{};
+      getInfoLog(shaderID, 512, nullptr, infoLog);
+      throw std::runtime_error{std::format("error: {} shader - {} failed\n{}\n", name, buildStage, infoLog)};
+    }
+  }
+
+  void check_linking_success(GLuint shaderID);
 }
