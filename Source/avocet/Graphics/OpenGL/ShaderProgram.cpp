6a015627b0f2a460e8205f8c13851d6ed1cf96d3
Oliver Rosten <oliver.rosten@gmail.com>
2024-09-16 14:45:05 +0100

    Explore a slightly different design

--- Source/avocet/Graphics/OpenGL/ShaderProgram.cpp
+++ Source/avocet/Graphics/OpenGL/ShaderProgram.cpp
@@ -12,39 +12,6 @@
 
 namespace avocet::opengl {
   namespace {
-    enum class shader_species : GLenum { vertex = GL_VERTEX_SHADER, fragment = GL_FRAGMENT_SHADER };
-
-    [[nodiscard]]
-    std::string to_string(shader_species species) {
-      using enum shader_species;
-      switch(species) {
-      case vertex:   return "vertex";
-      case fragment: return "fragment";
-      }
-
-      throw std::runtime_error{"shader_species: unexpected value"};
-    }
-
-    class shader_resource {
-      resource_handle m_Handle{};
-    public:
-      explicit shader_resource(shader_species species)
-        : m_Handle{glCreateShader(static_cast<GLenum>(species))}
-      {}
-
-      shader_resource(shader_resource&&) noexcept = default;
-
-      shader_resource& operator=(shader_resource&&) noexcept = default;
-
-      ~shader_resource() { glDeleteShader(m_Handle.index()); }
-
-      [[nodiscard]]
-      friend bool operator==(const shader_resource&, const shader_resource&) noexcept = default;
-
-      [[nodiscard]]
-      const resource_handle& handle() const noexcept { return m_Handle; }
-    };
-
     template<class GetStatus>
     GLint check_status(const resource_handle& handle, GLenum status, GetStatus getStatus) {
       GLint success{};
@@ -69,26 +36,6 @@ namespace avocet::opengl {
       check_success(resource.handle(), "program", "linking", GL_LINK_STATUS, glGetProgramiv, glGetProgramInfoLog);
     }
 
-    class shader_compiler {
-      shader_resource m_Resource;
-    public:
-      shader_compiler(shader_species species, std::string_view source)
-        : m_Resource{species}
-      {
-        const auto index{m_Resource.handle().index()};
-        const auto data{source.data()};
-        glShaderSource(index, 1, &data, nullptr);
-        glCompileShader(index);
-        check_compilation_success(m_Resource, to_string(species));
-      }
-
-      [[nodiscard]]
-      friend bool operator==(const shader_compiler&, const shader_compiler&) noexcept = default;
-
-      [[nodiscard]]
-      const shader_resource& resource() const noexcept { return m_Resource; }
-    };
-
     class [[nodiscard]] shader_attacher {
       GLuint m_ProgramIndex{}, m_ShaderIndex{};
     public:
@@ -102,6 +49,27 @@ namespace avocet::opengl {
         glDetachShader(m_ProgramIndex, m_ShaderIndex);
       }
     };
+
+    [[nodiscard]]
+    std::string to_string(shader_species species) {
+      using enum shader_species;
+      switch(species) {
+      case vertex:   return "vertex";
+      case fragment: return "fragment";
+      }
+
+      throw std::runtime_error{"shader_species: unexpected value"};
+    }
+  }
+
+  shader_compiler::shader_compiler(shader_species species, std::string_view source)
+    : m_Resource{species}
+  {
+    const auto index{m_Resource.handle().index()};
+    const auto data{source.data()};
+    glShaderSource(index, 1, &data, nullptr);
+    glCompileShader(index);
+    check_compilation_success(m_Resource, to_string(species));
   }
 
   shader_program::shader_program(std::string_view vertexShaderSource, std::string_view fragmentShaderSource) {
--- Source/avocet/Graphics/OpenGL/ShaderProgram.hpp
+++ Source/avocet/Graphics/OpenGL/ShaderProgram.hpp
@@ -10,6 +10,42 @@
 #include <string>
 
 namespace avocet::opengl {
+
+  enum class shader_species : GLenum { vertex = GL_VERTEX_SHADER, fragment = GL_FRAGMENT_SHADER };
+
+  class shader_resource {
+    resource_handle m_Handle{};
+  public:
+    explicit shader_resource(shader_species species)
+      : m_Handle{glCreateShader(static_cast<GLenum>(species))}
+    {}
+
+    shader_resource(shader_resource&&) noexcept = default;
+
+    shader_resource& operator=(shader_resource&&) noexcept = default;
+
+    ~shader_resource() { glDeleteShader(m_Handle.index()); }
+
+    [[nodiscard]]
+    friend bool operator==(const shader_resource&, const shader_resource&) noexcept = default;
+
+    [[nodiscard]]
+    const resource_handle& handle() const noexcept { return m_Handle; }
+  };
+
+  class shader_compiler {
+    shader_resource m_Resource;
+
+  public:
+    shader_compiler(shader_species species, std::string_view source);
+
+    [[nodiscard]]
+    friend bool operator==(const shader_compiler&, const shader_compiler&) noexcept = default;
+
+    [[nodiscard]]
+    const shader_resource& resource() const noexcept { return m_Resource; }
+  };
+
   class shader_program_resource {
     resource_handle m_Handle{};
   public:
@@ -32,6 +68,7 @@ namespace avocet::opengl {
 
   class shader_program {
     shader_program_resource m_Resource{};
+
   public:
     shader_program(std::string_view vertexShaderSource, std::string_view fragmentShaderSource);
 
