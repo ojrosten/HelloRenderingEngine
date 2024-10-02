044be9c1b42797549434c58eeca3f4027fb25ae4
Oliver Rosten <oliver.rosten@gmail.com>
2024-10-02 12:39:11 +0100

    Add pragrmas which somehow went missing!

--- Demo/TestingUtilities/GLFWWrappers.hpp
+++ Demo/TestingUtilities/GLFWWrappers.hpp
@@ -5,6 +5,8 @@
 //          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
 ////////////////////////////////////////////////////////////////////
 
+#pragma once
+
 struct GLFWwindow;
 
 namespace demo {
