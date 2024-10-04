7056d02bb969363e382290f9253e8f9e7e466279
Oliver Rosten <oliver.rosten@gmail.com>
2024-10-04 11:16:41 +0100

    Add some infrastructure to help handle VBOs etc

--- Source/avocet/Graphics/OpenGL/Buffers.hpp
+++ Source/avocet/Graphics/OpenGL/Buffers.hpp
@@ -9,6 +9,34 @@
 
 #include "avocet/Graphics/OpenGL/ResourceHandle.hpp"
 
-namespace avocet::opengl {
+#include <array>
 
+namespace avocet::opengl {
+    template<std::size_t N>
+    using raw_indices = std::array<GLuint, N>;
+
+    template<std::size_t N>
+    using handles = std::array<resource_handle, N>;
+
+    template<class From, class To, class Fn, std::size_t N>
+        requires std::is_invocable_r_v<To, Fn, From>
+    [[nodiscard]]
+    std::array<To, N> to_array(const std::array<From, N>& from, Fn fn) {
+        return
+            [&] <std::size_t... Is> (std::index_sequence<Is...>) {
+            return std::array<To, N>{fn(from[Is])...};
+        }(std::make_index_sequence<N>{});
+    }
+
+    template<std::size_t N>
+    [[nodiscard]]
+    handles<N> to_handle_array(const raw_indices<N>& indices) {
+        return to_array<GLuint, resource_handle>(indices, [](GLuint i){ return resource_handle{i}; });
+    }
+
+    template<std::size_t N>
+    [[nodiscard]]
+    raw_indices<N> to_index_array(const handles<N>& handles) {
+        return to_array<resource_handle, GLuint>(handles, [](const resource_handle& h){ return h.index(); });
+    }
 }
