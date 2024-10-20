f82aa7a178cc276ae1139090bee32fd6d509ccd2
Oliver Rosten <oliver.rosten@gmail.com>
2024-10-20 19:27:40 +0100

    Ensure test is never parallelized

--- Tests/Graphics/OpenGL/ShaderProgramFreeTest.hpp
+++ Tests/Graphics/OpenGL/ShaderProgramFreeTest.hpp
@@ -13,11 +13,15 @@
 
 namespace avocet::testing
 {
-    class shader_program_free_test final : public sequoia::testing::free_test
+    using namespace sequoia::testing;
+  
+    class shader_program_free_test final : public free_test
     {
-    public:
-        using sequoia::testing::free_test::free_test;
+    public:      
+        using parallelizable_type = std::false_type;
 
+        using free_test::free_test;
+      
         [[nodiscard]]
         std::filesystem::path source_file() const;
 
