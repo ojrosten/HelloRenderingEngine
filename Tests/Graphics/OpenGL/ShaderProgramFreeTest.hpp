545070e93ac4b894204b9994c62b5b9650abe9f4
Oliver Rosten <oliver.rosten@gmail.com>
2024-10-20 20:52:17 +0100

    Formatting

--- Tests/Graphics/OpenGL/ShaderProgramFreeTest.hpp
+++ Tests/Graphics/OpenGL/ShaderProgramFreeTest.hpp
@@ -14,14 +14,14 @@
 namespace avocet::testing
 {
     using namespace sequoia::testing;
-  
+
     class shader_program_free_test final : public free_test
     {
-    public:      
+    public:
         using parallelizable_type = std::false_type;
 
         using free_test::free_test;
-      
+
         [[nodiscard]]
         std::filesystem::path source_file() const;
 
