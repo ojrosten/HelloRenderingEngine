a2afeb01cd62d76ac3978060d6a978bb830a1c4f
Oliver Rosten <oliver.rosten@gmail.com>
2024-10-25 14:43:53 +0100

    Complete rollout of gl_function

--- Source/avocet/Geometry/OpenGL/Triangle.hpp
+++ Source/avocet/Geometry/OpenGL/Triangle.hpp
@@ -21,18 +21,18 @@ namespace avocet::opengl {
         vbo_resource m_VBO;
     public:
         triangle() {
-            glBindVertexArray(m_VAO.get_handle().index());
+            gl_function{glBindVertexArray}(m_VAO.get_handle().index());
 
-            glBindBuffer(GL_ARRAY_BUFFER, m_VBO.get_handle().index());
-            glBufferData(GL_ARRAY_BUFFER, sizeof(m_Vertices), m_Vertices.data(), GL_STATIC_DRAW);
+            gl_function{glBindBuffer}(GL_ARRAY_BUFFER, m_VBO.get_handle().index());
+            gl_function{glBufferData}(GL_ARRAY_BUFFER, sizeof(m_Vertices), m_Vertices.data(), GL_STATIC_DRAW);
 
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-            glEnableVertexAttribArray(0);
+            gl_function{glVertexAttribPointer}(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            gl_function{glEnableVertexAttribArray}(0);
         }
 
         void draw() {
-            glBindVertexArray(m_VAO.get_handle().index());
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            gl_function{glBindVertexArray}(m_VAO.get_handle().index());
+            gl_function{glDrawArrays}(GL_TRIANGLES, 0, 3);
         }
     };
 }
