d2eba25666ffee83222bd3ebdd6d7f3c15ee731a
Oliver Rosten <oliver.rosten@gmail.com>
2024-10-30 15:42:20 +0000

    Add first check to ShaderProgram test

--- Tests/Graphics/OpenGL/ShaderProgramFreeTest.cpp
+++ Tests/Graphics/OpenGL/ShaderProgramFreeTest.cpp
@@ -21,5 +21,21 @@ namespace avocet::testing
 
     void shader_program_free_test::run_tests()
     {
+        using namespace curlew;
+        glfw_manager manager{};
+        auto w{manager.create_window({.hiding{window_hiding_mode::on}})};
+
+        namespace agl = avocet::opengl;
+        const auto shaderDir{working_materials()};
+
+        check_exception_thrown<std::runtime_error>(
+            "Broken Vertex Shader",
+            [&shaderDir](){
+                agl::shader_program sp{
+                    shaderDir / "Broken_Identity.vs",
+                    shaderDir / "Monochrome.fs"
+                };
+            }
+        );
     }
 }
