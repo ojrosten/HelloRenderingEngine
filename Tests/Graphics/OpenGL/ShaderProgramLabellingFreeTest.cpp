ccba26cd09917dbd78f1b11b1839cfd0506496fa
Oliver Rosten <oliver.rosten@gmail.com>
2024-12-09 14:58:03 +0000

    Revert "Renaming"
    This reverts commit cf8b0ec1cc002ffef818193e5b41db6d33f861a1.

--- Tests/Graphics/OpenGL/ShaderProgramLabellingFreeTest.cpp
+++ Tests/Graphics/OpenGL/ShaderProgramLabellingFreeTest.cpp
@@ -7,7 +7,7 @@
 
 /*! \file */
 
-#include "ObjectLabellingFreeTest.hpp"
+#include "ShaderProgramLabellingFreeTest.hpp"
 #include "avocet/Graphics/OpenGL/ShaderProgram.hpp"
 #include "avocet/Utilities/OpenGL/Casts.hpp"
 
