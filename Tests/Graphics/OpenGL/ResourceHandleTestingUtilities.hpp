c4cf2f3d6729d457047f89b60a320bd283d712cb
Oliver Rosten <oliver.rosten@gmail.com>
2024-12-19 10:39:13 +0000

    Testing Infrastructure

---
+++ Tests/Graphics/OpenGL/ResourceHandleTestingUtilities.hpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2024.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+/*! \file */
+
+#include "sequoia/TestFramework/MoveOnlyTestCore.hpp"
+#include "avocet/Graphics/OpenGL/ResourceHandle.hpp"
+
+namespace sequoia::testing
+{
+    template<> struct value_tester<avocet::opengl::resource_handle>
+    {
+        using type = avocet::opengl::resource_handle;
+
+        template<test_mode Mode>
+        static void test(equality_check_t, test_logger<Mode>& logger, const type& actual, const type& prediction)
+        {
+        }
+        
+        template<test_mode Mode>
+        static void test(equivalence_check_t, test_logger<Mode>& logger, const type& actual, const GLuint& prediction)
+        {
+        }
+    };
+}
