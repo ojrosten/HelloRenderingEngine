377513d66ab57379bc450c001317b25651e7e931
Oliver Rosten <oliver.rosten@gmail.com>
2025-01-08 11:53:55 +0000

    Tweak

--- TestingUtilities/curlew/TestFramework/GraphicsLabellingTestCore.cpp
+++ TestingUtilities/curlew/TestFramework/GraphicsLabellingTestCore.cpp
@@ -33,12 +33,10 @@ namespace curlew {
     [[nodiscard]]
     std::string get_object_label(avocet::opengl::object_identifier identifier, const avocet::opengl::resource_handle& handle) {
         std::string label(get_max_label_length(), ' ');
-        agl::gl_function{glGetObjectLabel}(agl::to_gl_enum(identifier), handle.index(), agl::to_gl_sizei(label.size()), nullptr, label.data());
-        auto pos{std::ranges::find_if_not(std::views::reverse(label), [](auto c) { return c == ' '; }).base()};
-        if((pos != label.begin()) && (*(pos-1) == '\0'))
-            --pos;
+        GLsizei numChars{};
+        agl::gl_function{glGetObjectLabel}(agl::to_gl_enum(identifier), handle.index(), agl::to_gl_sizei(label.size()), &numChars, label.data());
 
-        label.erase(pos, label.end());
+        label.erase(std::ranges::next(label.begin(), numChars, label.end()), label.end());
 
         return label;
     }
