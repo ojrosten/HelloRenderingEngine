8ec6271f8df165a49a8e3b900c97733b06a9ace4
Oliver Rosten <oliver.rosten@gmail.com>
2025-01-24 12:08:13 +0000

    Start relocating code

--- Source/avocet/Graphics/OpenGL/Buffers.hpp
+++ Source/avocet/Graphics/OpenGL/Buffers.hpp
@@ -7,13 +7,9 @@
 
 #pragma once
 
-#include "avocet/Graphics/OpenGL/ObjectIdentifiers.hpp"
-#include "avocet/Graphics/OpenGL/ResourceHandle.hpp"
-#include "avocet/Graphics/OpenGL/GLFunction.hpp"
-#include "avocet/Utilities/OpenGL/Casts.hpp"
+#include "avocet/Graphics/OpenGL/Labels.hpp"
 
 #include <array>
-#include <optional>
 #include <ranges>
 #include <span>
 #include <vector>
@@ -92,47 +88,6 @@ namespace avocet::opengl {
             LifeEvents::configure(h, config);
         }
     };
-
-    using optional_label = std::optional<std::string>;
-    inline constexpr optional_label null_label{std::nullopt};
-
-    inline void add_label(object_identifier identifier, const resource_handle& h, const optional_label& label) {
-        if(label && object_labels_activated()) {
-            const auto& str{label.value()};
-            gl_function{glObjectLabel}(to_gl_enum(identifier), h.index(), to_gl_sizei(str.size()), str.data());
-        }
-    }
-
-    [[nodiscard]]
-    inline GLint get_max_label_length() {
-        const static GLint length{
-            [](){
-                GLint param{};
-                gl_function{glGetIntegerv}(GL_MAX_LABEL_LENGTH, &param);
-                return param;
-            }()
-        };
-
-        return length;
-    }
-
-    [[nodiscard]]
-    inline std::string get_object_label(avocet::opengl::object_identifier identifier, const avocet::opengl::resource_handle& handle) {
-        std::string label(get_max_label_length(), ' ');
-        GLsizei numChars{};
-        gl_function{glGetObjectLabel}(
-            to_gl_enum(identifier),
-            handle.index(),
-            to_gl_sizei(label.size()),
-            &numChars,
-            label.data()
-            );
-
-        label.erase(std::ranges::next(label.begin(), numChars, label.end()), label.end());
-
-        return label;
-    }
-
 
     struct vao_lifecycle_events {
         constexpr static auto identifier{object_identifier::vertex_array};
