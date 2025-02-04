a560acc6e43c61923e3849108fd4c6bd4e77d1b4
Oliver Rosten <oliver.rosten@gmail.com>
2025-02-04 12:24:50 +0000

    Testing Infrastructure

--- Tests/Graphics/OpenGL/BufferObjectTestingUtilities.hpp
+++ Tests/Graphics/OpenGL/BufferObjectTestingUtilities.hpp
@@ -14,4 +14,23 @@
 
 namespace sequoia::testing
 {
+    /*namespace agl = avocet::opengl;
+
+    struct gneric_buffer_object_tester {
+        template<test_mode Mode, agl::buffer_species Species, agl::gl_arithmetic_type T>
+        static void test(equivalence_check_t, test_logger<Mode>& logger, const agl::generic_buffer_object<Species, T>& buffer, const std::optional<std::span<const T>>& prediction) {
+            if(prediction) {
+                check(equality, "Buffer Data", logger, std::span<const T>{extract_data(buffer)}, prediction.value());
+            }
+            else {
+                check("Null Buffer", logger, buffer.is_null());
+            }
+        }
+    };
+
+    template<agl::gl_arithmetic_type T>
+    struct value_tester<agl::vertex_buffer_object<T>> : gneric_buffer_object_tester {};
+
+    template<agl::gl_arithmetic_type T>
+    struct value_tester<agl::element_buffer_object<T>> : gneric_buffer_object_tester {};*/
 }
