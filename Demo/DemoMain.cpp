0e9bff1bba344d283ea8d7ef7c3f59e25c414476
Oliver Rosten <oliver.rosten@gmail.com>
2025-03-20 17:27:30 +0000

    Render the septagon in 2D

--- Demo/DemoMain.cpp
+++ Demo/DemoMain.cpp
@@ -53,6 +53,7 @@ int main()
         namespace agl = avocet::opengl;
         agl::shader_program
             shaderProgram{get_shader_dir() / "Identity.vs", get_shader_dir() / "Monochrome.fs"},
+            shaderProgram2D{get_shader_dir() / "Identity2D.vs", get_shader_dir() / "Monochrome.fs"},
             shaderProgramDouble{get_shader_dir() / "IdentityDouble.vs", get_shader_dir() / "Monochrome.fs"};
 
         agl::polygon<GLfloat, 4, agl::dimensionality{3}> q{
@@ -71,20 +72,20 @@ int main()
             make_label("Quad")
         };
 
-        agl::polygon<GLfloat, 7, agl::dimensionality{3}> sept{
+        agl::polygon<GLfloat, 7, agl::dimensionality{2}> sept{
             [](std::ranges::random_access_range auto verts) {
                 // Won't work with libc++ (clang) until views::stride is available; fine on MSVC and gcc
-                //std::ranges::for_each(std::views::stride(verts, 3), [](auto& v){ v += 0.25; });
-                //std::ranges::for_each(std::views::drop(verts, 1) | std::views::stride(3), [](auto& v){ v -= 0.25; });
+                //std::ranges::for_each(std::views::stride(verts, 2), [](auto& v){ v += 0.25; });
+                //std::ranges::for_each(std::views::drop(verts, 1) | std::views::stride(2), [](auto& v){ v -= 0.25; });
 
                 for(auto i : std::views::iota(0, std::ssize(verts))) {
-                    if(!(i % 3))       verts[i] += 0.5;
-                    if(!((i - 1) % 3)) verts[i] -= 0.5;
+                    if(!(i % 2))       verts[i] += 0.5;
+                    if(!((i - 1) % 2)) verts[i] -= 0.5;
                 }
 
                 return verts;
             },
-            make_label("Quad")
+            make_label("Septagon")
         };
 
         agl::triangle<GLdouble, agl::dimensionality{3}> tri{
@@ -109,6 +110,7 @@ int main()
 
             shaderProgram.use();
             q.draw();
+            shaderProgram2D.use();
             sept.draw();
             shaderProgramDouble.use();
             tri.draw();
