9b456333aa4b5ca66dc75ea2a87d59167b78b34b
ojrosten <oliver.rosten@gmail.com>
2025-04-17 17:30:25 +0100

    Fix for gcc

--- Demo/DemoMain.cpp
+++ Demo/DemoMain.cpp
@@ -76,7 +76,7 @@ int main()
         constexpr GLfloat radius{0.5};
         constexpr std::array<GLfloat, 2> centre{-0.5, 0.5};
         agl::triangle<GLfloat, agl::dimensionality{2}> disc{
-            [](std::ranges::random_access_range auto verts) {
+            [&radius,&centre](std::ranges::random_access_range auto verts) {
                 // Won't work with libc++ (clang) until views::stride is available; fine on MSVC and gcc
                 //std::ranges::for_each(std::views::stride(verts, 3), [](auto& v){ v -= 0.5; });
                 //std::ranges::for_each(std::views::drop(verts, 1) | std::views::stride(3), [](auto& v){ v += 0.5; });
