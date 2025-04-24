cb02de355b39640812c0cc6441e989facdce4eea
Oliver Rosten <oliver.rosten@gmail.com>
2025-04-24 14:29:14 +0100

    Gamma correction

--- Demo/Shaders/Textured.fs
+++ Demo/Shaders/Textured.fs
@@ -1,11 +1,13 @@
 #version 330 core
-out vec4 FragColor;
+out vec4 FragColour;
 
 in vec2 TexCoords;
 
 uniform sampler2D image;
+uniform float gamma = 2.2;
 
 void main()
 {    
-    FragColor = texture(image, TexCoords);
+    FragColour     = texture(image, TexCoords);
+    FragColour.rgb = pow(FragColour.rgb, vec3(1.0/gamma));
 }
