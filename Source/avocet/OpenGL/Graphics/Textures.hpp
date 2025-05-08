6e25c8497b083268659537e7d279e1e72cf07efe
Oliver Rosten <oliver.rosten@gmail.com>
2025-05-08 15:16:29 +0100

    New files to be populated in lecture 22

---
+++ Source/avocet/OpenGL/Graphics/Textures.hpp
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2024.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "avocet/Core/AssetManagement/Image.hpp"
+
+#include "avocet/OpenGL/Graphics/Labels.hpp"
+#include "avocet/OpenGL/Graphics/Resources.hpp"
+#include "avocet/OpenGL/Utilities/TypeTraits.hpp"
+
+#include "sequoia/FileSystem/FileSystem.hpp"
+
+#include <array>
+#include <filesystem>
+#include <functional>
+#include <ranges>
+#include <span>
+#include <vector>
+
+namespace avocet::opengl {
+}
