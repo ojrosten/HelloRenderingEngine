a429a03a0c76d492afe96d63eeccfff4dcedb77d
Oliver Rosten <oliver.rosten@gmail.com>
2025-05-09 10:17:24 +0100

    Add texturing functionality

--- Source/avocet/OpenGL/Graphics/Textures.cpp
+++ Source/avocet/OpenGL/Graphics/Textures.cpp
@@ -8,4 +8,42 @@
 #include "avocet/OpenGL/Graphics/Textures.hpp"
 
 namespace avocet::opengl {
+    namespace {
+        struct image_format
+        {
+            GLint internal_format{};
+            GLenum format{};
+        };
+
+        [[nodiscard]]
+        image_format to_format(colour_space_flavour colourSpace, std::size_t numChannels)
+        {
+            const bool isLinear{colourSpace == colour_space_flavour::linear};
+            switch(numChannels)
+            {
+            case 1:
+                return {.internal_format{GL_RED},
+                        .format{GL_RED}};
+            case 2:
+                return {.internal_format{GL_RG},
+                        .format{GL_RG}};
+            case 3:
+                return {.internal_format{isLinear ? GL_RGB  : GL_SRGB},
+                        .format{GL_RGB}};
+            case 4:
+                return {.internal_format{isLinear ? GL_RGBA : GL_SRGB_ALPHA},
+                        .format{GL_RGBA}};
+            }
+
+            throw std::runtime_error{std::format("{} channels requested, but it must be in the range [1,4]", numChannels)};
+        }
+    }
+
+    void load_texture_2d_to_gpu(const texture_2d_configuration& config) {
+        image im{config.image_config.file(), config.image_config.flipped()};
+        const auto format{to_format(config.image_config.colour_space(), im.num_channels())};
+
+        gl_function{glTexImage2D}(GL_TEXTURE_2D, 0, format.internal_format, static_cast<int>(im.width()), static_cast<int>(im.height()), 0, format.format, GL_UNSIGNED_BYTE, im.span().data());
+        if(config.parameter_setter) config.parameter_setter();
+    }
 }
--- Source/avocet/OpenGL/Graphics/Textures.hpp
+++ Source/avocet/OpenGL/Graphics/Textures.hpp
@@ -23,4 +23,95 @@
 #include <vector>
 
 namespace avocet::opengl {
+    enum class colour_space_flavour : int { linear, srgb };
+
+    class image_configuration
+    {
+        std::filesystem::path m_File{};
+        flip_vertically m_Flip{};
+        colour_space_flavour m_ColourSpace{colour_space_flavour::srgb};
+    public:
+        image_configuration(std::filesystem::path textureFile, flip_vertically flip, colour_space_flavour colourSpace)
+            : m_File{std::move(textureFile)}
+            , m_Flip{flip}
+            , m_ColourSpace{colourSpace}
+        {}
+
+        [[nodiscard]]
+        const std::filesystem::path& file() const noexcept { return m_File; }
+
+        [[nodiscard]]
+        flip_vertically flipped() const noexcept { return  m_Flip; }
+
+        [[nodiscard]]
+        colour_space_flavour colour_space() const noexcept { return m_ColourSpace; }
+
+
+        [[nodiscard]]
+        friend bool operator==(const image_configuration&, const image_configuration&) = default;
+    };
+
+    struct default_texture_2d_parameter_setter {
+        void operator()() {
+            gl_function{glGenerateMipmap}(GL_TEXTURE_2D);
+            gl_function{glTexParameteri}(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            gl_function{glTexParameteri}(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            gl_function{glTexParameteri}(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            gl_function{glTexParameteri}(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        };
+    };
+
+    struct texture_2d_configuration {
+        constexpr static auto flavour{GL_TEXTURE_2D};
+
+        image_configuration image_config;
+        std::function<void()> parameter_setter{default_texture_2d_parameter_setter{}};
+        optional_label label{sequoia::back(image_config.file()).generic_string()};
+    };
+
+    void load_texture_2d_to_gpu(const texture_2d_configuration& config);
+
+    struct texture_lifecycle_events {
+        using configuration = texture_2d_configuration;
+
+        constexpr static auto identifier{object_identifier::texture};
+
+        template<std::size_t N>
+        static void generate(raw_indices<N>& indices) { gl_function{glGenTextures}(N, indices.data()); }
+
+        template<std::size_t N>
+        static void destroy(const raw_indices<N>& indices) { gl_function{glDeleteTextures}(N, indices.data()); }
+
+        static void bind(const resource_handle& h) { gl_function{glBindTexture}(GL_TEXTURE_2D, h.index()); }
+
+        static void configure(const resource_handle& h, const configuration& config) {
+            add_label(identifier, h, config.label);
+            load_texture_2d_to_gpu(config);
+        }
+    };
+
+    template<num_resources NumResources>
+    class texture_object : public generic_resource<NumResources, texture_lifecycle_events> {
+    public:
+        using base_type         = generic_resource<NumResources, texture_lifecycle_events>;
+        using configurator_type = base_type::configurator_type;
+        constexpr static auto N{base_type::N};
+
+        texture_object(const std::array<configurator_type, N>& textureConfig)
+            : base_type{textureConfig}
+        {}
+
+        friend void bind_for_rendering(const texture_object& texObj) {
+            [&] <std::size_t... Is>(std::index_sequence<Is...>){
+                (texture_object::activate_and_bind<Is>(texObj), ...);
+            }(std::make_index_sequence<N>{});
+        }
+
+    private:
+        template<std::size_t I>
+        static void activate_and_bind(const texture_object& texObj) {
+            gl_function{glActiveTexture}(GL_TEXTURE0 + I);
+            base_type::do_bind(texObj, index<I>{});
+        }
+    };
 }
