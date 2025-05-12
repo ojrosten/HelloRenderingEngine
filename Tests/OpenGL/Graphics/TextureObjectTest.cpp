29d90133fc0486ffccd4067ddee6bbd695ccc00d
Oliver Rosten <oliver.rosten@gmail.com>
2025-05-12 10:46:31 +0100

    More testing

--- Tests/OpenGL/Graphics/TextureObjectTest.cpp
+++ Tests/OpenGL/Graphics/TextureObjectTest.cpp
@@ -25,6 +25,20 @@ namespace avocet::testing
         using namespace curlew;
         glfw_manager manager{};
         auto w{manager.create_window({.hiding{window_hiding_mode::on}})};
+
+        using value_type = agl::texture_2d::value_type;
+        using opt_data = std::optional<avocet::image_view>;
+
+        std::vector<value_type> textureVals{42, 7, 6, 10}, textureVals2{255, 0, 0, 4, 0, 255, 0, 8};
+        check_semantics(
+            reporter{""},
+            agl::texture_2d{agl::texture_2d_configuration{.data{textureVals, 1, 1, 4}}},
+            agl::texture_2d{agl::texture_2d_configuration{.data{textureVals2, 2, 1, 4}}},
+            opt_data{avocet::image_view{textureVals, 1, 1, 4}},
+            opt_data{avocet::image_view{textureVals2, 2, 1, 4}},
+            opt_data{},
+            opt_data{avocet::image_view{textureVals, 1, 1, 4}}
+        );
     }
 
     [[nodiscard]]
@@ -35,5 +49,7 @@ namespace avocet::testing
 
     void texture_2d_object_labelling_free_test::labelling_tests()
     {
+
+        
     }
 }
