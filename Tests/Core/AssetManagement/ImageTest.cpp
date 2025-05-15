af463a5ca42406f19faa4a0f1c53dcd146cc079a
Oliver Rosten <oliver.rosten@gmail.com>
2025-05-15 11:53:15 +0100

    Update ImageTest.cpp

--- Tests/Core/AssetManagement/ImageTest.cpp
+++ Tests/Core/AssetManagement/ImageTest.cpp
@@ -47,7 +47,7 @@ namespace avocet::testing
             "",
             image{working_materials() / "red_2w_3h_3c.png",         flip_vertically::no,  all_channels_in_image},
             image{working_materials() / "bgr_striped_2w_3h_3c.png", flip_vertically::yes, all_channels_in_image},
-            make_red(2, 3, image_channels{3}, alignment{1}, 255),
+            make_red(        2, 3, image_channels{3}, alignment{1}, 255),
             make_rgb_striped(2, 3, image_channels{3}, alignment{1}),
             image_data{},
             image_data{}
