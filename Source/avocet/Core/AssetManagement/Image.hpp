caa3705f6ca089d2404a92caf322ed47c200a758
Oliver Rosten <oliver.rosten@gmail.com>
2025-05-29 14:32:23 +0100

    Pre-baked code for lecture 22

--- Source/avocet/Core/AssetManagement/Image.hpp
+++ Source/avocet/Core/AssetManagement/Image.hpp
@@ -99,6 +99,35 @@ namespace avocet {
     };
 
     class image_view {
+    /*public:
+        using value_type = unique_image::value_type;
 
+        image_view(const unique_image& im)
+            : m_Span{im.span()}
+            , m_Spec{.width{im.width()},
+                     .height{im.height()},
+                     .channels{im.num_channels()}}
+        {
+        }
+
+        [[nodiscard]]
+        std::size_t width() const noexcept { return m_Spec.width; }
+
+        [[nodiscard]]
+        std::size_t height() const noexcept { return m_Spec.height; }
+
+        [[nodiscard]]
+        colour_channels num_channels() const noexcept { return m_Spec.channels; }
+
+        [[nodiscard]]
+        std::span<const value_type> span() const noexcept { return m_Span; }
+
+        [[nodiscard]]
+        friend bool operator==(const image_view& lhs, const image_view& rhs) noexcept {
+            return (lhs.m_Spec == rhs.m_Spec) && std::ranges::equal(lhs.span(), rhs.span());
+        }
+    private:
+        std::span<const value_type> m_Span;
+        impl::image_spec<std::size_t, std::size_t, colour_channels> m_Spec;*/
     };
 }
