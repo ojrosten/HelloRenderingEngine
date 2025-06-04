b658c47f68535b174a22bc158f464fd3548aa58a
Oliver Rosten <oliver.rosten@gmail.com>
2025-06-04 16:14:05 +0100

    Add tests for the new unique_image constructor
    Note that there is some repetition in the check_semantics call. Improving this is a nice homework exercise.

--- Tests/Core/AssetManagement/UniqueImageTest.cpp
+++ Tests/Core/AssetManagement/UniqueImageTest.cpp
@@ -30,6 +30,16 @@ namespace avocet::testing
             image_data{}
         );
 
+        check_semantics(
+            "",
+            unique_image{make_red(2, 3, 3).data, 2, 3, colour_channels{3}},
+            unique_image{make_red(1, 4, 2).data, 1, 4, colour_channels{2}},
+            make_red(2, 3, 3),
+            make_red(1, 4, 2),
+            image_data{},
+            image_data{}
+        );
+
         check_exception_thrown<std::runtime_error>(
             "Absent image",
             [this](){
@@ -41,6 +51,14 @@ namespace avocet::testing
             "Invalid image",
             [this](){
                 return unique_image{working_materials() / "not_an_image.txt", flip_vertically::no};
+            }
+        );
+
+        check_exception_thrown<std::runtime_error>(
+            "Inconsistent image data",
+            [](){
+                using value_t = unique_image::value_type;
+                return unique_image{std::vector<value_t>{}, 2uz, 3uz, colour_channels{4}};
             }
         );
     }
