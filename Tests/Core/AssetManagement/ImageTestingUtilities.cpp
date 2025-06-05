3c2af9002620fd3a4e35e185752b276491e8a067
Oliver Rosten <oliver.rosten@gmail.com>
2025-06-05 17:57:32 +0100

    Corrections

--- Tests/Core/AssetManagement/ImageTestingUtilities.cpp
+++ Tests/Core/AssetManagement/ImageTestingUtilities.cpp
@@ -28,14 +28,15 @@ namespace avocet::testing {
         image_data make_image(std::size_t w, std::size_t h, colour_channels channels, alignment rowAlignment, Fn fn) {
             constexpr auto bytesPerChannel{sizeof(image_data::value_type)};
 
-            const auto paddedRowSize{padded_row_size(w, channels, rowAlignment, bytesPerChannel)};
-            const bool isPadded{paddedRowSize > padded_row_size(w, channels, alignment{1}, bytesPerChannel)};
+            const auto nominalRowSize{padded_row_size(w, channels, alignment{1}, bytesPerChannel)};
+            const auto paddedRowSize{ padded_row_size(w, channels, rowAlignment, bytesPerChannel)};
+            const auto paddingBytes{paddedRowSize - nominalRowSize};
             return {
                 .data{
                       std::views::iota(0u, paddedRowSize * h)
                     | std::views::transform(
                         [=](auto i) -> unsigned char {
-                            if(const bool paddingByte{isPadded && !((i + 1) % paddedRowSize)}; paddingByte)
+                            if(const bool paddingByte{paddingBytes && (i % paddedRowSize) >= nominalRowSize}; paddingByte)
                                 return 0;
 
                             const auto row{i / paddedRowSize};
