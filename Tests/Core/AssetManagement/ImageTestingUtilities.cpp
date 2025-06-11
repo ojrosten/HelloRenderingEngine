2d430484e0316bb8792b2519d19bd53aa8d0735b
Oliver Rosten <oliver.rosten@gmail.com>
2025-06-11 17:45:58 +0100

    Refactor make_red and mage_rgb_striped

--- Tests/Core/AssetManagement/ImageTestingUtilities.cpp
+++ Tests/Core/AssetManagement/ImageTestingUtilities.cpp
@@ -12,46 +12,61 @@
 #include <ranges>
 
 namespace avocet::testing {
+    namespace {
+        template<class Fn>
+            requires std::is_invocable_r_v<typename image_data::value_type, Fn, std::size_t, std::size_t>
+        [[nodiscard]]
+        image_data make_image(std::size_t w, std::size_t h, colour_channels channels, alignment rowAlignment, Fn fn) {
+            constexpr auto bytesPerChannel{sizeof(image_data::value_type)};
 
-    [[nodiscard]]
-    image_data make_red(std::size_t w, std::size_t h, colour_channels channels, alignment rowAlignment, monochrome_intensity intensity) {
-        return {
-            .data{
-                  std::views::iota(0u, w * h * channels.raw_value())
-                | std::views::transform(
-                    [=](auto i) -> unsigned char {
-                        const auto channelIndex{i % channels.raw_value()};
-                        if(channelIndex == 3)
-                            return intensity.alpha;
+            const std::size_t paddedRowSize {padded_row_size(w, channels, bytesPerChannel, rowAlignment)},
+                              nominalRowSize{padded_row_size(w, channels, bytesPerChannel, alignment{1})};
+            return {
+                .data{
+                      std::views::iota(0u, safe_image_size(paddedRowSize, h))
+                    | std::views::transform(
+                        [=](auto i) -> unsigned char {
+                            if(const bool paddingByte{i % paddedRowSize >= nominalRowSize}; paddingByte)
+                                return 0;
 
-                        return static_cast<unsigned char>(channelIndex ? 0 : intensity.red);
-                    }
-                  )
-                | std::ranges::to<std::vector>()
-            },
-            .width{w},
-            .height{h},
-            .num_channels{channels}
-        };
+                            const auto row{i / paddedRowSize};
+                            const auto channelIndex{(i - paddedRowSize * row) % channels.raw_value()};
+                            return fn(row, channelIndex);
+                        }
+                      )
+                    | std::ranges::to<std::vector>()
+                },
+                .width{w},
+                .height{h},
+                .num_channels{channels},
+                .row_alignment{rowAlignment}
+            };
+        }
     }
 
     [[nodiscard]]
-    image_data make_rgb_striped(std::size_t w, std::size_t h, colour_channels channels, alignment rowAlignment) {
-        return {
-            .data{
-                  std::views::iota(0u, w * h * channels.raw_value())
-                | std::views::transform(
-                    [=](auto i) -> unsigned char {
-                        const auto row{i / (w * channels.raw_value())};
-                        const auto channelIndex{i % channels.raw_value()};
-                        return static_cast<unsigned char>((row % channels.raw_value()) == channelIndex ? 255 : 0);
-                    }
-                  )
-                | std::ranges::to<std::vector>()
-            },
-            .width{w},
-            .height{h},
-            .num_channels{channels}
+    image_data make_red(std::size_t w, std::size_t h, colour_channels channels, alignment rowAlignment, monochrome_intensity intensity) {
+        auto fn{
+            [intensity](std::size_t, std::size_t channelIndex){
+                if(channelIndex == 3)
+                     return intensity.alpha;
+
+                return static_cast<unsigned char>(channelIndex ? 0 : intensity.red);
+            }
         };
-    };
+
+        return make_image(w, h, channels, rowAlignment, fn);
+    }
+
+    [[nodiscard]]
+    [[nodiscard]]
+    image_data make_rgb_striped(std::size_t w, std::size_t h, colour_channels channels, alignment rowAlignment) {
+        auto fn{
+            [channels](std::size_t row, std::size_t channelIndex) {
+                return static_cast<unsigned char>((row % channels.raw_value()) == channelIndex ? 255 : 0);
+            }
+        };
+
+        return make_image(w, h, channels, rowAlignment, fn);
+    }
 }
