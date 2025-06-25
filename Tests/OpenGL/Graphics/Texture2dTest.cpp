3c74a3a3c043944ceaac65f8148ff8dd0f2ea7d9
Oliver Rosten <oliver.rosten@gmail.com>
2025-06-25 17:00:55 +0100

    Additional tests added, plus existing test tweaked to match description

--- Tests/OpenGL/Graphics/Texture2dTest.cpp
+++ Tests/OpenGL/Graphics/Texture2dTest.cpp
@@ -39,28 +39,48 @@ namespace avocet::testing
             {.image{.data{42, 6, 3, 8, 9, 1, 0, 0}, .width{2}, .height{1}, .num_channels{3}, .row_alignment{4}}, .decoding{}, .label{}}
         );
 
+        check_semantics_via_texture_data(
+            "Faithful roundtrip red and rg: data sent and received naturally aligned",
+            {.image{.data{42, 7, 10},         .width{3}, .height{1}, .num_channels{1}, .row_alignment{1}}, .decoding{}, .label{}},
+            {.image{.data{31, 9, 2, 6, 1, 8}, .width{1}, .height{3}, .num_channels{2}, .row_alignment{2}}, .decoding{}, .label{}}
+        );
 
-        // "Faithful roundtrip red and rg: data sent and received naturally aligned"
+        check_semantics_via_texture_data(
+            "Faithfully aligned roundtrip red and rg: data is sent and received padded",
+            {.image{.data{42, 7, 10, 0},            .width{3}, .height{1}, .num_channels{1}, .row_alignment{4}}, .decoding{}, .label{}},
+            {.image{.data{31, 9, 0, 0, 2, 6, 0, 0}, .width{1}, .height{2}, .num_channels{2}, .row_alignment{4}}, .decoding{}, .label{}}
+        );
 
-
-        // "Faithfully aligned roundtrip red and rg: data is sent and received padded"
-
-
-        // "Faithul colours but different paddings: red data is sent with padding and extracted without; rg vice-versa"
         check_semantics_via_texture_data(
              "Faithul colours but different paddings: red data is sent with padding and extracted without; rg vice-versa",
             {.image{.data{42, 0, 0, 0},             .width{1}, .height{1}, .num_channels{1}, .row_alignment{4}}, .decoding{}, .label{}},
                    {.data{42},                      .width{1}, .height{1}, .num_channels{1}, .row_alignment{1}},
-            {.image{.data{42, 6, 0, 0, 9, 1, 0, 0}, .width{1}, .height{2}, .num_channels{2}, .row_alignment{4}}, .decoding{}, .label{}},
-                   {.data{42, 6,       9, 1},       .width{1}, .height{2}, .num_channels{2}, .row_alignment{1}}
+            {.image{.data{42, 6,       9, 1},       .width{1}, .height{2}, .num_channels{2}, .row_alignment{2}}, .decoding{}, .label{}},
+                   {.data{42, 6, 0, 0, 9, 1, 0, 0}, .width{1}, .height{2}, .num_channels{2}, .row_alignment{4}}
         );
 
-        // "Channel Widening extractions: red -> rg with green=0; rgb -> rgba with alpha=255"
+        check_semantics_via_texture_data(
+            "Channel Widening extractions: red -> rg with green=0; rgb -> rgba with alpha=255",
+            {.image{.data{42, 7, 10},                   .width{3}, .height{1}, .num_channels{1}, .row_alignment{1}}, .decoding{}, .label{}},
+                   {.data{42, 0, 7, 0, 10, 0},          .width{3}, .height{1}, .num_channels{2}, .row_alignment{1}},
+            {.image{.data{31, 9, 2, 6, 4, 5},           .width{1}, .height{2}, .num_channels{3}, .row_alignment{1}}, .decoding{}, .label{}},
+                   {.data{31, 9, 2, 255, 6, 4, 5, 255}, .width{1}, .height{2}, .num_channels{4}, .row_alignment{4}}
+        );
 
+        check_semantics_via_texture_data(
+            "Channel Narrowing extractions: rg -> red; rgba -> rgb",
+            {.image{.data{42, 7, 10, 2},              .width{2}, .height{1}, .num_channels{2}, .row_alignment{2}}, .decoding{}, .label{}},
+                   {.data{42,    10   },              .width{2}, .height{1}, .num_channels{1}, .row_alignment{1}},
+            {.image{.data{31, 9, 2, 13, 6, 4, 5, 12}, .width{1}, .height{2}, .num_channels{4}, .row_alignment{1}}, .decoding{}, .label{}},
+                   {.data{31, 9, 2,     6, 4, 5    }, .width{1}, .height{2}, .num_channels{3}, .row_alignment{1}}
+        );
 
-        // "Channel Narrowing extractions: rg -> red; rgba -> rgb"
-
-
-        // "Mixed extractions: red -> rg but with alignment going from 4 -> 2; rgba -> rgb with alignment going from 2 -> 8"
+        check_semantics_via_texture_data(
+            "Mixed extractions: red -> rg but with alignment going from 4 -> 2; rgba -> rgb with alignment going from 2 -> 8",
+            {.image{.data{42, 7, 0, 0},                    .width{2}, .height{1}, .num_channels{1}, .row_alignment{4}}, .decoding{}, .label{}},
+                   {.data{42, 0, 7, 0},                    .width{2}, .height{1}, .num_channels{2}, .row_alignment{2}},
+            {.image{.data{31, 9, 2, 13, 6, 4, 5, 12},      .width{2}, .height{1}, .num_channels{4}, .row_alignment{2}}, .decoding{}, .label{}},
+                   {.data{31, 9, 2,     6, 4, 5,    0, 0}, .width{2}, .height{1}, .num_channels{3}, .row_alignment{8}}
+        );
     }
 }
