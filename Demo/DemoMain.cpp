01ef44ea15217261f3b48d32c97bb68991363841
Oliver Rosten <oliver.rosten@gmail.com>
2025-07-04 14:21:33 +0100

    Remove some comments; the point has been made!

--- Demo/DemoMain.cpp
+++ Demo/DemoMain.cpp
@@ -59,10 +59,6 @@ int main()
 
         agl::quad<GLdouble, agl::dimensionality{3}> q{
             [](std::ranges::random_access_range auto verts) {
-                // Won't work with libc++ (clang) until views::stride is available; fine on MSVC and gcc
-                //std::ranges::for_each(std::views::stride(verts, 3), [](auto& v){ v += 0.5; });
-                //std::ranges::for_each(std::views::drop(verts, 1) | std::views::stride(3), [](auto& v){ v -= 0.5; });
-
                 for(auto i : std::views::iota(0, std::ssize(verts))) {
                     if(!(i % 3))       verts[i] += 0.5;
                     if(!((i - 1) % 3)) verts[i] -= 0.5;
@@ -94,10 +90,6 @@ int main()
 
         agl::polygon<GLfloat, 7, agl::dimensionality{3}> sept{
             [](std::ranges::random_access_range auto verts) {
-                // Won't work with libc++ (clang) until views::stride is available; fine on MSVC and gcc
-                //std::ranges::for_each(std::views::stride(verts, 3), [](auto& v){ v += 0.5; });
-                //std::ranges::for_each(std::views::drop(verts, 1) | std::views::stride(3), [](auto& v){ v += 0.5; });
-
                 for(auto i : std::views::iota(0, std::ssize(verts))) {
                     if(!(i % 3))     verts[i] += 0.5;
                     if(!((i-1) % 3)) verts[i] += 0.5;
@@ -110,10 +102,6 @@ int main()
 
         agl::polygon<GLfloat, 6, agl::dimensionality{2}> hex{
             [](std::ranges::random_access_range auto verts) {
-                // Won't work with libc++ (clang) until views::stride is available; fine on MSVC and gcc
-                //std::ranges::for_each(std::views::stride(verts, 2), [](auto& v){ v -= 0.5; });
-                //std::ranges::for_each(std::views::drop(verts, 1) | std::views::stride(2), [](auto& v){ v -= 0.5; });
-
                 for(auto i : std::views::iota(0, std::ssize(verts))) {
                     if(!(i % 2))     verts[i] -= 0.5;
                     if(!((i-1) % 2)) verts[i] -= 0.5;
