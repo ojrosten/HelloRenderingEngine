d0f88b538d394e7783df4ab6790d26a2bb3d9c56
Oliver Rosten <oliver.rosten@gmail.com>
2025-07-10 19:32:33 +0100

    Update Textured.fs

--- Demo/Shaders/Textured.fs
+++ Demo/Shaders/Textured.fs
@@ -7,7 +7,7 @@ uniform sampler2D image;
 uniform float gamma = 2.2;
 
 void main()
-{    
+{
     FragColour     = texture(image, TexCoords);
     FragColour.rgb = pow(FragColour.rgb, vec3(1.0/gamma));
 }
