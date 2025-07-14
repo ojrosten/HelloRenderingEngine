c2000918ea76893c153a210a217b1085fe2fc6d9
Oliver Rosten <oliver.rosten@gmail.com>
2025-07-14 14:02:47 +0100

    Add a bit more infrastructure

--- Demo/DemoMain.cpp
+++ Demo/DemoMain.cpp
@@ -23,17 +23,23 @@ namespace {
     namespace fs = std::filesystem;
 
     [[nodiscard]]
-    fs::path get_shader_dir() {
+    fs::path get_dir(std::string_view dirName) {
         if(const fs::path file{std::source_location::current().file_name()}; file.is_absolute()) {
-            if(const auto dir{file.parent_path() / "Shaders"}; fs::exists(dir)) {
+            if(const auto dir{file.parent_path() / dirName}; fs::exists(dir)) {
                 return dir;
             }
             else
-                throw std::runtime_error{std::format("Unable to find shader directory {}", dir.generic_string())};
+                throw std::runtime_error{std::format("Unable to find {} directory {}", dirName, dir.generic_string())};
         }
 
         throw std::runtime_error{"Relative paths not supported"};
     }
+
+    [[nodiscard]]
+    fs::path get_shader_dir() { return get_dir("Shaders"); }
+
+    [[nodiscard]]
+    fs::path get_image_dir() { return get_dir("Images"); }
 
     [[nodiscard]]
     std::string make_label(std::string_view name, std::source_location loc = std::source_location::current()) {
