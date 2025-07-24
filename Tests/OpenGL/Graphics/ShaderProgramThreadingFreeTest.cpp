902e27325542d168b3ece3ecf3c31f9067c7d187
Oliver Rosten <oliver.rosten@gmail.com>
2025-07-24 18:03:06 +0100

    Bounded context for the other checks

--- Tests/OpenGL/Graphics/ShaderProgramThreadingFreeTest.cpp
+++ Tests/OpenGL/Graphics/ShaderProgramThreadingFreeTest.cpp
@@ -11,6 +11,7 @@
 #include "curlew/Window/GLFWWrappers.hpp"
 #include "avocet/OpenGL/Graphics/ShaderProgram.hpp"
 
+#include <future>
 #include <memory>
 #include <thread>
 
@@ -47,38 +48,50 @@ namespace avocet::testing
         glfw_manager manager{};
         const auto shaderDir{working_materials()};
 
-        const auto
-            prog0{make_and_use_shader_program(manager, shaderDir)},
-            prog1{make_and_use_shader_program(manager, shaderDir)};
-
-        if(check("Bounded Context", (!prog1) || (prog0 == prog1)))
         {
-            check("prog0 should report > 0", prog0 > 0);
-            check("prog1 should report > 0", prog1 > 0);
+            const auto
+                prog0{make_and_use_shader_program(manager, shaderDir)},
+                prog1{make_and_use_shader_program(manager, shaderDir)};
+
+            if(check("Bounded Context", (!prog1) || (prog0 == prog1)))
+            {
+                check("prog0 should report > 0", prog0 > 0);
+                check("prog1 should report > 0", prog1 > 0);
+            }
         }
 
-        std::unique_ptr<agl::shader_program> spPtr{};
-
         {
-            std::jthread worker{
+            std::unique_ptr<agl::shader_program> spPtr{};
+            std::packaged_task<GLint(void)>  task0{
                 [&, this]() {
                     auto w{manager.create_window({.hiding{window_hiding_mode::on}})};
                     spPtr = std::make_unique<agl::shader_program>(shaderDir / "Identity.vs", shaderDir / "Monochrome.fs");
                     spPtr->use();
-                    check("", get_program_index() > 0);
+                    return get_program_index();
                 }
             };
-        }
 
-        {
-            std::jthread worker{
+            auto future0{task0.get_future()};
+
+            std::packaged_task<GLint(void)> task1{
                 [&, this]() {
                     auto w{manager.create_window({.hiding{window_hiding_mode::on}})};
                     agl::shader_program sp{shaderDir / "Identity.vs", shaderDir / "Monochrome.fs"};
                     sp.use();
-                    check("", get_program_index() > 0);
+                    return get_program_index();
                 }
             };
+
+            auto future1{task1.get_future()};
+
+            std::jthread worker0{std::move(task0)}, worker1{std::move(task1)};
+
+            const auto prog0{future0.get()}, prog1{future1.get()};
+            if(check("Bounded Context", (!prog1) || (prog0 == prog1)))
+            {
+                check("prog0 should report > 0", prog0 > 0);
+                check("prog1 should report > 0", prog1 > 0);
+            }
         }
     }
 }
