fcb4eaeafc228b0cfbd0e05a007fb3a757901fa4
Oliver Rosten <oliver.rosten@gmail.com>
2025-08-04 12:55:03 +0100

    Add infrastructure for the shader program tracker test

--- Tests/OpenGL/Graphics/ShaderProgramTrackingFreeTest.cpp
+++ Tests/OpenGL/Graphics/ShaderProgramTrackingFreeTest.cpp
@@ -18,6 +18,42 @@
 
 namespace avocet::testing
 {
+    namespace agl = avocet::opengl;
+    namespace fs = std::filesystem;
+
+    namespace
+    {
+        agl::resource_handle get_current_program_index() {
+            GLint param{};
+            agl::gl_function{glGetIntegerv}(GL_CURRENT_PROGRAM, &param);
+            if(param < 0)
+                throw std::runtime_error{std::format("Negative program index: {}", param)};
+
+            return agl::resource_handle{static_cast<GLuint>(param)};
+        }
+
+        agl::resource_handle make_and_use_shader_program(curlew::glfw_manager& manager,
+                                                         const fs::path& shaderDir,
+                                                         std::latch* pLatch) {
+            auto w{manager.create_window({.hiding{curlew::window_hiding_mode::on}})};
+
+            agl::shader_program sp{shaderDir / "Identity.vs", shaderDir / "Monochrome.fs"};
+            sp.use();
+
+            if(pLatch) pLatch->arrive_and_wait();
+
+            return get_current_program_index();
+        }
+
+        constexpr std::latch* no_latch{};
+
+        [[nodiscard]]
+        std::string make_description(std::string_view tag, std::string_view description)
+        {
+            return std::format("{}: {}", tag, description);
+        }
+    }
+
     [[nodiscard]]
     std::filesystem::path shader_program_tracking_free_test::source_file() const
     {
@@ -42,6 +78,9 @@ namespace avocet::testing
 
     void shader_program_tracking_free_test::check_program_indices(const avocet::opengl::resource_handle& prog0, const avocet::opengl::resource_handle& prog1, std::string_view tag)
     {
+        check(make_description(tag, "prog0 should not be null"), prog0 != agl::resource_handle{});
+        check(make_description(tag, "prog1 should not be null"), prog1 != agl::resource_handle{});
 
+        check(make_description(tag, "Assumption required for sensitivity to program 0 utilization accidentally suppressing program 1 utilization"), prog0 == prog1);
     }
 }
