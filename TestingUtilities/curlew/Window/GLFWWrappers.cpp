ojrosten/HelloRenderingEngine

commit fa937c38dbe80f0ba1d1407bc24e046cad4fd0cc

Clarify the logic when trying to create a context with the maximum allowed OpenGL version
This is adapted for the platforms on which we're actually working:

Windows - always 4.6
Apple - always 4.1
Linux - various

TestingUtilities/curlew/Window/GLFWWrappers.cpp
@@ -93,13 +93,15 @@ namespace curlew {
 
     [[nodiscard]]
     rendering_setup glfw_manager::do_find_rendering_setup() const {
-      const auto setup{attempt_to_find_rendering_setup(agl::opengl_version{})};
-      if((setup.version != agl::opengl_version{}) || avocet::is_apple())
-          return setup;
+        constexpr agl::opengl_version trialVersion{.major{4}, .minor{avocet::is_windows() ? 6 : 1}};
 
-      // Assume we only get here if we're on windows, in which case
-      // the version is 4.6. If this ever fails in practice, it can be fixed.
-      return attempt_to_find_rendering_setup(agl::opengl_version{.major{4}, .minor{6}});
+        const auto setup{attempt_to_find_rendering_setup(trialVersion)};
+        if(avocet::is_windows() || avocet::is_apple()) {
+            if(setup.version != trialVersion)
+                throw std::runtime_error{std::format("On {} expected OpenGL version {} but found {}", avocet::is_windows() ? "Windows" : "Apple", trialVersion, setup.version)};
+        }
+
+        return setup;
     }
 
     [[nodiscard]]

commit b6fc15936a54567a5c89fa8593806d2cc44ac5ef

Fix the logic for finding the maximum supported OpenGL version
This code was broken by commit fa937c38dbe80f0ba1d1407bc24e046cad4fd0cc

TestingUtilities/curlew/Window/GLFWWrappers.cpp
@@ -100,6 +100,14 @@ namespace curlew {
             if(setup.version != trialVersion)
                 throw std::runtime_error{std::format("On {} expected OpenGL version {} but found {}", avocet::is_windows() ? "Windows" : "Apple", trialVersion, setup.version)};
         }
+        else if(setup.version == trialVersion) {
+            // On some (but not all) linux systems, requesting a trial version of 4.1 will return
+            // a version of 4.1, even if higher OpenGL versions are available. However, all such
+            // systems I've encountered to date happen to support 4.6. If the latter turns out not
+            // to be the case somewhere, than the following will need to be amended, to divine the
+            // actual maximum supported version
+            return attempt_to_find_rendering_setup(agl::opengl_version{.major{4}, .minor{6}});
+        }
 
         return setup;
     }