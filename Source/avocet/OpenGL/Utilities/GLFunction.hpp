ojrosten/HelloRenderingEngine

commit d2650bbd4318e6bb165682ae3390e5964f16218a

Renaming

Source/avocet/OpenGL/Utilities/GLFunction.hpp
@@ -24,17 +24,17 @@ namespace avocet::opengl {
     using function_pointer_type = R(*)(Args...);
 
     template<class R, class... Args>
-    using glad_ctx_ptr_to_mem_fn_ptr_type = function_pointer_type<R, Args...> GladGLContext::*;
+    using glad_ctx_ptr_to_mem_PtrToMem_ptr_type = function_pointer_type<R, Args...> GladGLContext::*;
 
     template<class R, class... Args, debugging_mode Mode>
     class [[nodiscard]] gl_function<R(Args...), Mode> {
     public:
-        using pointer_to_member_type = glad_ctx_ptr_to_mem_fn_ptr_type<R, Args...>;
+        using pointer_to_member_type = glad_ctx_ptr_to_mem_PtrToMem_ptr_type<R, Args...>;
 
         constexpr static num_messages max_reported_messages{10};
 
         gl_function(pointer_to_member_type ptrToMem)
-            : m_Fn{ptrToMem}
+            : m_PtrToMem{ptrToMem}
         {}
 
         gl_function(unchecked_debug_output_t, pointer_to_member_type ptrToMem)
@@ -49,7 +49,7 @@ namespace avocet::opengl {
 
         [[nodiscard]]
         R operator()(const GladGLContext& ctx, Args... args, std::source_location loc = std::source_location::current()) const {
-            const auto ret{validate(ctx, loc)(args...)};
+            const auto ret{get_validated_fn_ptr(ctx, loc)(args...)};
             check_for_errors(ctx, loc);
             return ret;
         }
@@ -57,15 +57,15 @@ namespace avocet::opengl {
         void operator()(const GladGLContext& ctx, Args... args, std::source_location loc = std::source_location::current()) const
             requires std::is_void_v<R>
         {
-            validate(ctx, loc)(args...);
+            get_validated_fn_ptr(ctx, loc)(args...);
             check_for_errors(ctx, loc);
         }
     private:
-        pointer_to_member_type m_Fn;
+        pointer_to_member_type m_PtrToMem;
 
         [[nodiscard]]
-        function_pointer_type<R, Args...> validate(const GladGLContext& ctx, std::source_location loc) const {
-            auto f{ctx.*m_Fn};
+        function_pointer_type<R, Args...> get_validated_fn_ptr(const GladGLContext& ctx, std::source_location loc) const {
+            auto f{ctx.*m_PtrToMem};
             return f ? f : throw std::runtime_error{std::format("gl_function: attempting to construct with a nullptr coming via {}", to_string(loc))};
         }
         static void check_for_errors(const GladGLContext& ctx, std::source_location loc) {
@@ -79,8 +79,8 @@ namespace avocet::opengl {
     };
 
     template<class R, class... Args>
-    gl_function(glad_ctx_ptr_to_mem_fn_ptr_type<R, Args...>) -> gl_function<R(Args...)>;
+    gl_function(glad_ctx_ptr_to_mem_PtrToMem_ptr_type<R, Args...>) -> gl_function<R(Args...)>;
 
     template<class R, class...Args>
-    gl_function(unchecked_debug_output_t, glad_ctx_ptr_to_mem_fn_ptr_type<R, Args...>) -> gl_function<R(Args...), debugging_mode::none>;
+    gl_function(unchecked_debug_output_t, glad_ctx_ptr_to_mem_PtrToMem_ptr_type<R, Args...>) -> gl_function<R(Args...), debugging_mode::none>;
 }

Source/avocet/OpenGL/Resources/GenericResource.hpp
@@ -73,7 +73,7 @@ namespace avocet::opengl {
         resource_wrapper& operator=(resource_wrapper&&) noexcept = default;
 
         [[nodiscard]]
-        const contextual_resource_handles<N>& get_handles() const noexcept { return m_Handles; }
+        const contextual_resource_handles<N>& contextual_handles() const noexcept { return m_Handles; }
 
         [[nodiscard]]
         friend bool operator==(const resource_wrapper&, const resource_wrapper&) noexcept = default;
@@ -94,32 +94,32 @@ namespace avocet::opengl {
         explicit generic_resource(const GladGLContext& ctx, const std::array<configurator_type, N>& configs)
             : m_Resource{ctx}
         {
-            for(const auto& [handle, config] : std::views::zip(get_handles(), configs)) {
-                if(handle == contextual_resource_handle{ctx, resource_handle{}})
+            for(const auto& [ctxHandle, config] : std::views::zip(contextual_handles(), configs)) {
+                if(ctxHandle.handle() == resource_handle{})
                     throw std::runtime_error{"generic_resource  - null resource"};
 
-                lifecycle_type::bind(handle);
-                lifecycle_type::configure(handle, config);
+                lifecycle_type::bind(ctxHandle);
+                lifecycle_type::configure(ctxHandle, config);
             }
         }
 
         [[nodiscard]]
         bool is_null() const noexcept {
-            auto isNull{[](const contextual_resource_handle& h) { return h == contextual_resource_handle{h.context(), resource_handle{}}; }};
-            assert(std::ranges::all_of(get_handles(), isNull) or std::ranges::none_of(get_handles(), isNull));
-            return isNull(get_handle(index<0>{}));
+            auto isNull{[](const contextual_resource_handle& h) { return h.handle() == resource_handle{}; }};
+            assert(std::ranges::all_of(contextual_handles(), isNull) or std::ranges::none_of(contextual_handles(), isNull));
+            return isNull(contextual_handle(index<0>{}));
         }
 
         template<std::size_t I>
             requires (I < N)
         [[nodiscard]]
-        std::string extract_label(index<I> i) const { return get_object_label(LifeEvents::identifier, get_handle(i)); }
+        std::string extract_label(index<I> i) const { return get_object_label(LifeEvents::identifier, contextual_handle(i)); }
 
         [[nodiscard]]
         std::string extract_label() const requires (N == 1) { return extract_label(index<0>{}); }
 
         [[nodiscard]]
-        const GladGLContext& context() const noexcept { return m_Resource.get_handles().context(); }
+        const GladGLContext& context() const noexcept { return contextual_handles().context(); }
 
         [[nodiscard]]
         friend bool operator==(const generic_resource&, const generic_resource&) noexcept = default;
@@ -131,15 +131,15 @@ namespace avocet::opengl {
 
         template<std::size_t I>
             requires (I < N)
-        static void do_bind(const generic_resource& gbo, index<I> i) { lifecycle_type::bind(gbo.get_handle(i)); }
+        static void do_bind(const generic_resource& gbo, index<I> i) { lifecycle_type::bind(gbo.contextual_handle(i)); }
 
         static void do_bind(const generic_resource& gbo) requires (N == 1) { do_bind(gbo, index<0>{}); }
     private:
         [[nodiscard]]
-        const contextual_resource_handles<N>& get_handles() const noexcept { return m_Resource.get_handles(); }
+        const contextual_resource_handles<N>& contextual_handles() const noexcept { return m_Resource.contextual_handles(); }
 
         template<std::size_t I>
             requires (I < N)
-        const contextual_resource_handle& get_handle(index<I>) const noexcept { return get_handles().begin()[I]; }
+        const contextual_resource_handle& contextual_handle(index<I>) const noexcept { return contextual_handles().begin()[I]; }
     };
 }

commit 9d98401c965b1c807b2751f5bf4f3646cfeaec03

Correct a find-and-replace mistake introduced in d2650bbd4318e6bb165682ae3390e5964f16218a

Source/avocet/OpenGL/Utilities/GLFunction.hpp
@@ -24,12 +24,12 @@ namespace avocet::opengl {
     using function_pointer_type = R(*)(Args...);
 
     template<class R, class... Args>
-    using glad_ctx_ptr_to_mem_PtrToMem_ptr_type = function_pointer_type<R, Args...> GladGLContext::*;
+    using glad_ctx_ptr_to_mem_ptr_to_mem_ptr_type = function_pointer_type<R, Args...> GladGLContext::*;
 
     template<class R, class... Args, debugging_mode Mode>
     class [[nodiscard]] gl_function<R(Args...), Mode> {
     public:
-        using pointer_to_member_type = glad_ctx_ptr_to_mem_PtrToMem_ptr_type<R, Args...>;
+        using pointer_to_member_type = glad_ctx_ptr_to_mem_ptr_to_mem_ptr_type<R, Args...>;
 
         constexpr static num_messages max_reported_messages{10};
 
@@ -79,8 +79,8 @@ namespace avocet::opengl {
     };
 
     template<class R, class... Args>
-    gl_function(glad_ctx_ptr_to_mem_PtrToMem_ptr_type<R, Args...>) -> gl_function<R(Args...)>;
+    gl_function(glad_ctx_ptr_to_mem_ptr_to_mem_ptr_type<R, Args...>) -> gl_function<R(Args...)>;
 
     template<class R, class...Args>
-    gl_function(unchecked_debug_output_t, glad_ctx_ptr_to_mem_PtrToMem_ptr_type<R, Args...>) -> gl_function<R(Args...), debugging_mode::none>;
+    gl_function(unchecked_debug_output_t, glad_ctx_ptr_to_mem_ptr_to_mem_ptr_type<R, Args...>) -> gl_function<R(Args...), debugging_mode::none>;
 }