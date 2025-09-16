b6f0a099c50859c575505aee747424cccee4e52f
Oliver Rosten <oliver.rosten@gmail.com>
2025-09-16 17:56:40 +0100

    Improved error message

--- Source/avocet/OpenGL/Utilities/GLFunction.hpp
+++ Source/avocet/OpenGL/Utilities/GLFunction.hpp
@@ -47,22 +47,28 @@ namespace avocet::opengl {
 
         [[nodiscard]]
         R operator()(const decorated_context& ctx, Args... args, std::source_location loc = std::source_location::current()) const {
-            const auto& fptr{get_validated_fn_ptr(ctx, loc)};
-            return ctx.invoke_decorated({Mode, get_name(ctx.glad_context(), fptr), loc}, fptr, args...);
+            const auto [fptr, name]{get_validated_fn_ptr(ctx, loc)};
+            return ctx.invoke_decorated({Mode, name, loc}, fptr, args...);
         }
 
     private:
         pointer_to_member_type m_PtrToMem;
 
+        struct validated_fn_ptr {
+            function_pointer_type<R, Args...> f;
+            std::string_view name;
+        };
+
         [[nodiscard]]
-        const function_pointer_type<R, Args...>& get_validated_fn_ptr(const decorated_context& ctx, std::source_location loc) const {
+        validated_fn_ptr get_validated_fn_ptr(const decorated_context& ctx, std::source_location loc) const {
             const auto& f{ctx.glad_context().*m_PtrToMem};
-            return f ? f : throw std::runtime_error{std::format("gl_function: attempting to invoke a null function pointer coming via {}", to_string(loc))};
+            std::string_view name{get_name(ctx, f)};
+            return f ? validated_fn_ptr{f, name} : throw std::runtime_error{std::format("gl_function: attempting to invoke a null function pointer {} coming via {}", name, to_string(loc))};
         }
 
         [[nodiscard]]
-        std::string_view get_name(const GladGLContext& ctx, const function_pointer_type<R, Args...>& fptr) const {
-            const auto offset{std::bit_cast<uintptr_t>(&fptr) - std::bit_cast<uintptr_t>(&ctx)};
+        std::string_view get_name(const decorated_context& ctx, const function_pointer_type<R, Args...>& fptr) const {
+            const auto offset{std::bit_cast<uintptr_t>(&fptr) - std::bit_cast<uintptr_t>(&ctx.glad_context())};
             const auto index{(offset - glad_ctx_member_info[0].offset) / sizeof(void*)};
             static_assert((glad_ctx_member_info.back().offset - glad_ctx_member_info.front().offset) / sizeof(int*) == (glad_ctx_member_info.size() - 1));
             if(index >= glad_ctx_member_info.size())
