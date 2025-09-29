48dd23ac0aa69f7183eb14d9dccec49199a970dd
Oliver Rosten <oliver.rosten@gmail.com>
2025-09-29 11:50:52 +0100

    Supply the skeleton of two shaders that will be amended and then used in lecture 31

---
+++ Demo/Shaders/Fragment/General/MixedTextures.fs
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColour;
+
+in vec2 TexCoords;
+
+uniform sampler2D image;
+uniform float gamma = 2.2;
+
+void main()
+{
+    FragColour     = texture(image, TexCoords);
+    FragColour.rgb = pow(FragColour.rgb, vec3(1.0/gamma));
+}
