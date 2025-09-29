4d33a23b472093817c4164965bfaf0eecb5cdb1e
Oliver Rosten <oliver.rosten@gmail.com>
2025-09-29 13:59:14 +0100

    Likely content for lecture 31

--- Demo/Shaders/Fragment/General/MixedTextures.fs
+++ Demo/Shaders/Fragment/General/MixedTextures.fs
@@ -1,13 +1,16 @@
 #version 330 core
 out vec4 FragColour;
 
-in vec2 TexCoords;
+in vec2 TexCoords0;
+in vec2 TexCoords1;
 
-uniform sampler2D image;
+uniform sampler2D image0;
+uniform sampler2D image1;
 uniform float gamma = 2.2;
+uniform float weight = 0.5;
 
 void main()
 {
-    FragColour     = texture(image, TexCoords);
+    FragColour     = mix(texture(image0, TexCoords0), texture(image1, TexCoords1), weight);
     FragColour.rgb = pow(FragColour.rgb, vec3(1.0/gamma));
 }
--- Demo/Shaders/Vertex/2D/IdentityTwiceTextured.vs
+++ Demo/Shaders/Vertex/2D/IdentityTwiceTextured.vs
@@ -1,12 +1,15 @@
 #version 330 core
 
 layout (location = 0) in vec2 aLocalPos;
-layout (location = 1) in vec2 aTexCoords;
+layout (location = 1) in vec2 aTexCoords0;
+layout (location = 2) in vec2 aTexCoords1;
 
-out vec2 TexCoords;
+out vec2 TexCoords0;
+out vec2 TexCoords1;
 
 void main()
 {
     gl_Position = vec4(aLocalPos, 0.0, 1.0);
-    TexCoords   = aTexCoords;
+    TexCoords0  = aTexCoords0;
+    TexCoords1  = aTexCoords1;
 }
