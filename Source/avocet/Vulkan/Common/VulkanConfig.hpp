254ecb24527bb310404eb965be521f6125e11aa0
Oliver Rosten <oliver.rosten@gmail.com>
2025-12-24 16:07:46 +0000

    Tidying

--- Source/avocet/Vulkan/Common/VulkanConfig.hpp
+++ Source/avocet/Vulkan/Common/VulkanConfig.hpp
@@ -12,5 +12,5 @@
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #define VULKAN_HPP_ENABLE_DYNAMIC_LOADER_TOOL 0
 
-#include <volk.h>
-#include <vulkan/vulkan_raii.hpp>
+#include "volk.h"
+#include "vulkan/vulkan_raii.hpp"
