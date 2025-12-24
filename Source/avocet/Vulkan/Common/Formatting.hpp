6b697836c4ef5051e563a0cb54bbcd77652dd830
Oliver Rosten <oliver.rosten@gmail.com>
2025-12-24 16:07:37 +0000

    Missing pragmas

--- Source/avocet/Vulkan/Common/Formatting.hpp
+++ Source/avocet/Vulkan/Common/Formatting.hpp
@@ -5,6 +5,8 @@
 //          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
 ////////////////////////////////////////////////////////////////////
 
+#pragma once
+
 #include "avocet/Core/Formatting/Formatting.hpp"
 
 #include "avocet/Vulkan/Common/VulkanConfig.hpp"
--- TestingUtilities/curlew/Window/VulkanInitialization.hpp
+++ TestingUtilities/curlew/Window/VulkanInitialization.hpp
@@ -5,6 +5,8 @@
 //          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
 ////////////////////////////////////////////////////////////////////
 
+#pragma once
+
 #include "avocet/Vulkan/Presentation/Presentation.hpp"
 
 namespace curlew::vulkan {
