7b4e2d64867a882b0342005bbc6be4eeee491b24
Oliver Rosten <oliver.rosten@gmail.com>
2026-01-23 12:04:57 +0000

    Infrastructure for a more general approach to the viewport
    We mimic Vulkan by using a fixed-width unsigned type for extents. This differs from the OpenGL model, which uses GLsizei.

---
+++ Source/avocet/Core/RenderArea/Viewport.cpp
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2026.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#include "avocet/Core/RenderArea/Viewport.hpp"
+
+namespace avocet {
+    namespace {
+        [[nodiscard]]
+        bool good_extent(const discrete_extent& extent) {
+            return (extent.width > 0) && (extent.height > 0);
+        }
+    }
+
+    [[nodiscard]]
+    std::optional<viewport> refit(const discrete_extent& framebufferExtent, const discrete_extent& requestedExtent) {
+        return null_viewport;
+    }
+}
---
+++ Source/avocet/Core/RenderArea/Viewport.hpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2026.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <optional>
+#include <cstdint>
+
+namespace avocet {
+
+    struct discrete_offset {
+        std::int32_t x{}, y{};
+
+        [[nodiscard]]
+        friend constexpr bool operator==(const discrete_offset&, const discrete_offset&) noexcept = default;
+    };
+
+    struct discrete_extent {
+        std::uint32_t width{}, height{};
+
+        [[nodiscard]]
+        friend constexpr bool operator==(const discrete_extent&, const discrete_extent&) noexcept = default;
+    };
+
+    struct viewport {
+        discrete_offset offset;
+        discrete_extent extent;
+
+        [[nodiscard]]
+        friend constexpr bool operator==(const viewport&, const viewport&) noexcept = default;
+    };
+
+    [[nodiscard]]
+    std::optional<viewport> refit(const discrete_extent& framebufferExtent, const discrete_extent& requestedExtent);
+
+    inline constexpr std::optional<viewport> null_viewport{};
+}
