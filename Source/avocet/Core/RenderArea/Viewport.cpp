b2bd71190c28e5e8e540c06063e0246500cb14de
Oliver Rosten <oliver.rosten@gmail.com>
2026-01-27 12:33:03 +0000

    Content for lecture 39

--- Source/avocet/Core/RenderArea/Viewport.cpp
+++ Source/avocet/Core/RenderArea/Viewport.cpp
@@ -27,6 +27,30 @@ namespace avocet {
     /// </summary>
     [[nodiscard]]
     std::optional<viewport> refit(const discrete_extent& nominalExtent, const discrete_extent& availableExtent) {
+        if(!good_extent(nominalExtent) || !good_extent(availableExtent))
+            return null_viewport;
+
+        const double  nominalAspectRatio{static_cast<double>(nominalExtent.width) / nominalExtent.height},
+            availableAspectRatio{static_cast<double>(availableExtent.width) / availableExtent.height};
+
+        if(availableAspectRatio < nominalAspectRatio) {
+            const std::uint32_t width{availableExtent.width};
+            const std::uint32_t height{static_cast<std::uint32_t>(width / nominalAspectRatio)};
+            const std::int32_t  delta_y{static_cast<std::int32_t>((availableExtent.height - height) / 2)};
+            if(height > 0)
+                return viewport{{0, delta_y}, {width, height}};
+        }
+        else if(availableAspectRatio > nominalAspectRatio) {
+            const std::uint32_t height{availableExtent.height};
+            const std::uint32_t width{static_cast<std::uint32_t>(height * nominalAspectRatio)};
+            const std::int32_t  delta_x{static_cast<std::int32_t>((availableExtent.width - width) / 2)};
+            if(width > 0)
+                return viewport{{delta_x, 0}, {width, height}};
+        }
+        else {
+            return viewport{{}, availableExtent};
+        }
+
         return null_viewport;
     }
 }
--- Tests/Core/RenderArea/ViewportTest.cpp
+++ Tests/Core/RenderArea/ViewportTest.cpp
@@ -11,18 +11,6 @@
 
 namespace avocet::testing
 {
-    namespace {
-        [[nodiscard]]
-        constexpr bool good_extent(const discrete_extent& extent) noexcept {
-            return (extent.width > 0) && (extent.height > 0);
-        }
-
-        [[nodiscard]]
-        std::optional<viewport> refit(const discrete_extent& nominalExtent, const discrete_extent& availableExtent) {
-            return null_viewport;
-        }
-    }
-
     using opt_viewport = std::optional<avocet::viewport>;
 
     [[nodiscard]]
@@ -40,5 +28,27 @@ namespace avocet::testing
     }
 
     void viewport_test::test_refit() {
+        check(equality, "", refit({0, 1}, {1, 1}), null_viewport);
+        check(equality, "", refit({1, 0}, {1, 1}), null_viewport);
+        check(equality, "", refit({1, 1}, {0, 1}), null_viewport);
+        check(equality, "", refit({1, 1}, {1, 0}), null_viewport);
+
+        check(equality, "Unable to exactly centralize", refit({1, 1}, {1, 2}), opt_viewport{{{}, {1, 1}}});
+        check(equality, "", refit({1, 1}, {1, 3}), opt_viewport{{{0, 1}, {1, 1}}});
+        check(equality, "Computed height of zero", refit({2, 1}, {1, 1}), null_viewport);
+        check(equality, "", refit({1, 1}, {2, 3}), opt_viewport{{{0, 0}, {2, 2}}});
+        check(equality, "", refit({2, 2}, {1, 3}), opt_viewport{{{0, 1}, {1, 1}}});
+        check(equality, "Unable to respect aspect ratio", refit({5, 2}, {4, 3}), opt_viewport{{{0, 1}, {4, 1}}});
+
+        check(equality, "Unable to exactly centralize", refit({1, 1}, {2, 1}), opt_viewport{{{}, {1, 1}}});
+        check(equality, "", refit({1, 1}, {3, 1}), opt_viewport{{{1, 0}, {1, 1}}});
+        check(equality, "Computed width of zero", refit({1, 2}, {1, 1}), null_viewport);
+        check(equality, "", refit({1, 1}, {3, 2}), opt_viewport{{{0, 0}, {2, 2}}});
+        check(equality, "", refit({2, 2}, {3, 1}), opt_viewport{{{1, 0}, {1, 1}}});
+        check(equality, "Unable to respect aspect ratio", refit({2, 5}, {3, 4}), opt_viewport{{{1, 0}, {1, 4}}});
+
+        check(equality, "", refit({1, 1}, {1, 1}), opt_viewport{{{0, 0}, {1, 1}}});
+        check(equality, "", refit({1, 1}, {2, 2}), opt_viewport{{{0, 0}, {2, 2}}});
+        check(equality, "", refit({2, 2}, {1, 1}), opt_viewport{{{0, 0}, {1, 1}}});
     }
 }
