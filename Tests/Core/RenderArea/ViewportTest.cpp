d2e49d149a33b229c96f3d59e50c37c05ad55ecd
Oliver Rosten <oliver.rosten@gmail.com>
2026-01-29 11:46:20 +0000

    Add some additional tests, plus format the code
    Note: these are the tests I said I'd add during the live-coding. However, there remain some edge cases which are not tested, so this commit does not interfere with the homework problem!

--- Tests/Core/RenderArea/ViewportTest.cpp
+++ Tests/Core/RenderArea/ViewportTest.cpp
@@ -13,31 +13,6 @@ namespace avocet::testing
 {
     using opt_viewport = std::optional<avocet::viewport>;
 
-    void viewport_test::test_refit() {
-        check(equality, "", refit({0, 1}, {1, 1}), null_viewport);
-        check(equality, "", refit({1, 0}, {1, 1}), null_viewport);
-        check(equality, "", refit({1, 1}, {0, 1}), null_viewport);
-        check(equality, "", refit({1, 1}, {1, 0}), null_viewport);
-
-        check(equality, "Unable to exactly centralize", refit({1, 1}, {1, 2}), opt_viewport{{{0, 0}, {1, 1}}});
-        check(equality, "", refit({1, 1}, {1, 3}), opt_viewport{{{0, 1}, {1, 1}}});
-        check(equality, "Computed height of zero", refit({2, 1}, {1, 1}), null_viewport);
-        check(equality, "Expanding: Unable to exactly centralize", refit({1, 1}, {2, 3}), opt_viewport{{{0, 0}, {2, 2}}});
-        check(equality, "", refit({2, 2}, {1, 3}), opt_viewport{{{0, 1}, {1, 1}}});
-        check(equality, "Unable to preserve aspect ratio", refit({5, 2}, {4, 3}), opt_viewport{{{0, 1}, {4, 1}}});
-
-        check(equality, "Unable to exactly centralize", refit({1, 1}, {2, 1}), opt_viewport{{{}, {1, 1}}});
-        check(equality, "", refit({1, 1}, {3, 1}), opt_viewport{{{1, 0}, {1, 1}}});
-        check(equality, "Computed height of zero", refit({1, 2}, {1, 1}), null_viewport);
-        check(equality, "Expanding: unable to exactly centralize", refit({1, 1}, {3, 2}), opt_viewport{{{0, 0}, {2, 2}}});
-        check(equality, "", refit({2, 2}, {3, 1}), opt_viewport{{{1, 0}, {1, 1}}});
-        check(equality, "Contracting: unable to respect aspect ratio", refit({2, 5}, {3, 4}), opt_viewport{{{1, 0}, {1, 4}}});
-
-        check(equality, "", refit({1, 1}, {1, 1}), opt_viewport{{{0, 0}, {1, 1}}});
-        check(equality, "", refit({2, 2}, {1, 1}), opt_viewport{{{0, 0}, {1, 1}}});
-        check(equality, "", refit({1, 1}, {2, 2}), opt_viewport{{{0, 0}, {2, 2}}});
-    }
-
     [[nodiscard]]
     std::filesystem::path viewport_test::source_file() const
     {
@@ -50,5 +25,36 @@ namespace avocet::testing
         check_semantics("", x, y);
 
         test_refit();
+    }
+
+    void viewport_test::test_refit() {
+        check(equality, "", refit({0, 1}, {1, 1}), null_viewport);
+        check(equality, "", refit({1, 0}, {1, 1}), null_viewport);
+        check(equality, "", refit({1, 1}, {0, 1}), null_viewport);
+        check(equality, "", refit({1, 1}, {1, 0}), null_viewport);
+
+        check(equality, "Unable to exactly centralize"                , refit({1, 1}, {1, 2}), opt_viewport{{{0, 0}, {1, 1}}});
+        check(equality, "Centralized"                                 , refit({1, 1}, {1, 3}), opt_viewport{{{0, 1}, {1, 1}}});
+        check(equality, "Computed height of zero"                     , refit({2, 1}, {1, 1}), null_viewport);
+        check(equality, "Expanding  : Unable to exactly centralize"   , refit({1, 1}, {2, 3}), opt_viewport{{{0, 0}, {2, 2}}});
+        check(equality, "Expanding  : Centralized"                    , refit({1, 2}, {2, 6}), opt_viewport{{{0, 1}, {2, 4}}});
+        check(equality, "Contracting: Unable to exactly centralize"   , refit({3, 3}, {2, 3}), opt_viewport{{{0, 0}, {2, 2}}});
+        check(equality, "Contracting: Centralized"                    , refit({2, 4}, {1, 4}), opt_viewport{{{0, 1}, {1, 2}}});
+        check(equality, "Expanding  : Unable to preserve aspect ratio", refit({3, 2}, {4, 4}), opt_viewport{{{0, 1}, {4, 2}}});
+        check(equality, "Contracting: Unable to preserve aspect ratio", refit({5, 2}, {4, 3}), opt_viewport{{{0, 1}, {4, 1}}});
+
+        check(equality, "Unable to exactly centralize"                , refit({1, 1}, {2, 1}), opt_viewport{{{}, {1, 1}}});
+        check(equality, "Centralized"                                 , refit({1, 1}, {3, 1}), opt_viewport{{{1, 0}, {1, 1}}});
+        check(equality, "Computed width of zero"                      , refit({1, 2}, {1, 1}), null_viewport);
+        check(equality, "Expanding  : Unable to exactly centralize"   , refit({1, 1}, {3, 2}), opt_viewport{{{0, 0}, {2, 2}}});
+        check(equality, "Expanding  : Centralized"                    , refit({2, 1}, {6, 2}), opt_viewport{{{1, 0}, {4, 2}}});
+        check(equality, "Contracting: Unable to exactly centralize"   , refit({3, 3}, {3, 2}), opt_viewport{{{0, 0}, {2, 2}}});
+        check(equality, "Contracting: Centralized"                    , refit({4, 2}, {4, 1}), opt_viewport{{{1, 0}, {2, 1}}});
+        check(equality, "Expanding  : Unable to preserve aspect ratio", refit({2, 3}, {4, 4}), opt_viewport{{{1, 0}, {2, 4}}});
+        check(equality, "Contracting: unable to preserve aspect ratio", refit({2, 5}, {3, 4}), opt_viewport{{{1, 0}, {1, 4}}});
+
+        check(equality, "", refit({1, 1}, {1, 1}), opt_viewport{{{0, 0}, {1, 1}}});
+        check(equality, "", refit({2, 2}, {1, 1}), opt_viewport{{{0, 0}, {1, 1}}});
+        check(equality, "", refit({1, 1}, {2, 2}), opt_viewport{{{0, 0}, {2, 2}}});
     }
 }
