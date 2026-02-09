c8cab951350ef0d2970bd15b18eb847cf50c9ba6
Oliver Rosten <oliver.rosten@gmail.com>
2026-02-09 17:09:25 +0000

    Create a skeleton of the first iteration of the new framebuffer_object class
    The new class is accompanied by a test which fails since the framebuffer is incomplete. This will be fixed during lecture 40.

---
+++ Source/avocet/OpenGL/Resources/Framebuffer.hpp
@@ -0,0 +1,60 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2026.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "avocet/OpenGL/Resources/Textures.hpp"
+
+namespace avocet::opengl {
+
+    struct framebuffer_lifecycle_events {
+        struct configurator {
+            optional_label label;
+        };
+
+        constexpr static auto identifier{object_identifier::framebuffer};
+
+        template<std::size_t N>
+        static void generate(const decorated_context& ctx, raw_indices<N>& indices) {
+            gl_function{&GladGLContext::GenFramebuffers}(ctx, N, indices.data());
+        }
+
+        template<std::size_t N>
+        static void destroy(const decorated_context& ctx, const raw_indices<N>& indices) {
+            gl_function{&GladGLContext::DeleteFramebuffers}(ctx, N, indices.data());
+        }
+
+        static void bind(contextual_resource_view h) {
+            gl_function{&GladGLContext::BindFramebuffer}(h.context(), GL_FRAMEBUFFER, get_index(h));
+        }
+
+        static void configure(contextual_resource_view h, const configurator& config) {
+            add_label(identifier, h, config.label);
+        }
+    };
+
+    using fbo_configurator = framebuffer_lifecycle_events::configurator;
+
+    class framebuffer_object : public generic_resource<num_resources{1}, framebuffer_lifecycle_events> {
+        void check_framebuffer_status();
+    public:
+        using base_type            = generic_resource<num_resources{1}, framebuffer_lifecycle_events>;
+
+        framebuffer_object(const decorated_context& ctx, const fbo_configurator& fboConfig)
+            : base_type{ctx, {{fboConfig.label}}}
+        {
+            check_framebuffer_status();
+        }
+
+        [[nodiscard]]
+        unique_image extract_data(this const framebuffer_object& self, texture_format format, alignment rowAlignment);
+
+        void bind(this const framebuffer_object& self, texture_unit unit) {
+            base_type::do_bind(self);
+        }
+    };
+}
---
+++ Tests/OpenGL/Resources/FramebufferFreeTest.cpp
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2026.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+/*! \file */
+
+#include "FramebufferFreeTest.hpp"
+#include "avocet/OpenGL/Resources/Framebuffer.hpp"
+
+#include "Core/AssetManagement/ImageTestingUtilities.hpp"
+
+namespace avocet::testing
+{
+    [[nodiscard]]
+    std::filesystem::path framebuffer_free_test::source_file() const
+    {
+        return std::source_location::current().file_name();
+    }
+
+    void framebuffer_free_test::run_tests()
+    {
+        using namespace curlew;
+        using namespace opengl;
+
+        constexpr avocet::discrete_extent extent{.width{1}, .height{2}};
+        auto w{create_window({.extent{extent}, .hiding{window_hiding_mode::on}})};
+
+        framebuffer_object
+            fbo{
+                w.context(),
+                fbo_configurator{.label{}}
+            };
+    }
+}
