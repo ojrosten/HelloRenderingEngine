c484042c639bec271a42b5399b0e3ce97b930aa5
Oliver Rosten <oliver.rosten@gmail.com>
2026-03-05 17:32:55 +0000

    Add a basic test for the generation of polygon vertices
    There is an interesting philosophical problem here. As it stands, the test effectively replicates a special case of the mathematics to be tested. This is not ideal, since the testing is rather tautological. In general, such approaches are vulnerable to the author having a blind spot that manifests the same in two bits of effectively identical code.
    
    The alternative is to perform the computations in a different way. The triangle vertices can be written as
    
    (0.5, 0), (sqrt(3)/4, -1/4), (-sqrt(3)/4, -1/4)
    
    The problem with this is that floating-point differences now cause the test to fail! Nevertheless, sequoia supports a solution to this which will be exhibited in the next commit.

--- Tests/Core/Geometry/PolygonCoordinatesFreeTest.cpp
+++ Tests/Core/Geometry/PolygonCoordinatesFreeTest.cpp
@@ -10,6 +10,12 @@
 #include "PolygonCoordinatesFreeTest.hpp"
 #include "avocet/Core/Geometry/PolygonCoordinates.hpp"
 
+// TO DO: These need to be made more naturally available from sequoia
+#include "sequoia/../../Tests/Maths/Geometry/GeometryTestingUtilities.hpp"
+#include "sequoia/../../Tests/Core/DataStructures/MemOrderedTupleTestingUtilities.hpp"
+
+#include <ranges>
+
 namespace avocet::testing
 {
     [[nodiscard]]
@@ -20,5 +26,13 @@ namespace avocet::testing
 
     void polygon_coordinates_free_test::run_tests()
     {
+        using poly_maker_t = make_polygon<float, 3, dimensionality{2}>;
+        using verts_t      = poly_maker_t::vertices_type;
+
+        const float x1{-0.5f * std::sin(2.0f * std::numbers::pi_v<float> / 3.0f)};
+        const float y1{ 0.5f * std::cos(2.0f * std::numbers::pi_v<float> / 3.0f)};
+        const float x2{-0.5f * std::sin(4.0f * std::numbers::pi_v<float> / 3.0f)};
+        const float y2{ 0.5f * std::cos(4.0f * std::numbers::pi_v<float> / 3.0f)};
+        check(equality, "", poly_maker_t{}(), verts_t{{{{0.0f, 0.5f}}, {{x1, y1}}, {{x2, y2}}}});
     }
 }
