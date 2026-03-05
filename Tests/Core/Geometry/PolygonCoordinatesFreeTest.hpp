635372f61a4a27ea0c143d49b84b8ad561585115
Oliver Rosten <oliver.rosten@gmail.com>
2026-03-05 17:54:15 +0000

    Testing for texture coordinates

--- Tests/Core/Geometry/PolygonCoordinatesFreeTest.hpp
+++ Tests/Core/Geometry/PolygonCoordinatesFreeTest.hpp
@@ -24,5 +24,9 @@ namespace avocet::testing
         std::filesystem::path source_file() const;
 
         void run_tests();
+    private:
+        void check_triangle();
+
+        void check_textured_triangle();
     };
 }
