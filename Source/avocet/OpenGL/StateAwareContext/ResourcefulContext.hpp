a849b39ecc5b911896e0549a044e5fa4376015a8
Oliver Rosten <oliver.rosten@gmail.com>
2026-04-20 14:47:14 +0100

    Formatting

--- Source/avocet/OpenGL/StateAwareContext/ResourcefulContext.hpp
+++ Source/avocet/OpenGL/StateAwareContext/ResourcefulContext.hpp
@@ -70,7 +70,6 @@ namespace avocet::opengl {
 
         template<class LifeEvents>
             requires has_bind_event_v<LifeEvents> || has_use_event_v<LifeEvents>
-
         void utilize(this const resourceful_context & self, const LifeEvents& lifeEvents, const resource_handle& h) {
             if constexpr(use_tracking_cache_v<LifeEvents>) {
                 constexpr auto id{LifeEvents::tracking_id};
