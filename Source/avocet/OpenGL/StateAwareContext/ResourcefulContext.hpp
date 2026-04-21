27eaa17e26d93ff6c6f646da1bb66914676e3fef
Oliver Rosten <oliver.rosten@gmail.com>
2026-04-21 12:00:37 +0100

    Remove code duplication

--- Source/avocet/OpenGL/StateAwareContext/ResourcefulContext.hpp
+++ Source/avocet/OpenGL/StateAwareContext/ResourcefulContext.hpp
@@ -49,9 +49,9 @@ namespace avocet::opengl {
 
         using tuple_t
             = std::tuple<
-                index_cache<caching_identifier_constant<caching_identifier::framebuffer>>,
-                index_cache<caching_identifier_constant<caching_identifier::program>>
-            >;
+                  index_cache<caching_identifier_constant<caching_identifier::framebuffer>>,
+                  index_cache<caching_identifier_constant<caching_identifier::program>>
+              >;
 
         mutable tuple_t m_IndexCache;
 
@@ -64,9 +64,12 @@ namespace avocet::opengl {
 
         template<class LifeEvents>
         constexpr static bool has_cache_v{
-            requires {
-                requires sequoia::meta::contains_v<tuple_t, index_cache<caching_identifier_constant<LifeEvents::caching_id>>>;
-            }
+            sequoia::meta::contains_v<tuple_t, index_cache<caching_identifier_constant<LifeEvents::caching_id>>>
+        };
+
+        template<class LifeEvents>
+        constexpr static bool has_utilization_event_v{
+            has_bind_event_v<LifeEvents> || has_use_event_v<LifeEvents>
         };
 
         friend class shader_program;
@@ -75,16 +78,11 @@ namespace avocet::opengl {
         friend class generic_resource;
 
         template<class LifeEvents>
-            requires has_bind_event_v<LifeEvents> || has_use_event_v<LifeEvents>
+            requires has_utilization_event_v<LifeEvents> 
         void utilize(this const resourceful_context & self, const LifeEvents& lifeEvents, const resource_handle& h) {
             if constexpr(opts_in_to_caching_v<LifeEvents>) {
-                static_assert(has_cache_v<LifeEvents>);
-
-                constexpr auto id{LifeEvents::caching_id};
-                auto& cache{std::get<index_cache<caching_identifier_constant<id>>>(self.m_IndexCache)};
-                if(cache.currently_active != h.index()) {
-                    self.do_utilize(lifeEvents, h);
-                    cache.currently_active = h.index();
+                if(auto& cache{self.get_cache(lifeEvents)}; cache.currently_active != h.index()) {
+                    self.cached_utlization(lifeEvents, cache, h);
                 }
             }
             else {
@@ -93,23 +91,36 @@ namespace avocet::opengl {
         }
 
         template<class LifeEvents>
-            requires has_bind_event_v<LifeEvents> || has_use_event_v<LifeEvents>
+            requires has_utilization_event_v<LifeEvents>
         void reset(this const resourceful_context& self, const LifeEvents& lifeEvents, const resource_handle& h) {
             if constexpr(opts_in_to_caching_v<LifeEvents>) {
-                static_assert(has_cache_v<LifeEvents>);
-
-                constexpr auto id{LifeEvents::caching_id};
-                auto& cache{std::get<index_cache<caching_identifier_constant<id>>>(self.m_IndexCache)};
-                if(cache.currently_active == h.index()) {
-                    self.do_utilize(lifeEvents, resource_handle{});
-                    cache.currently_active = 0;
+                if(auto& cache{self.get_cache(lifeEvents)}; cache.currently_active == h.index()) {
+                    self.cached_utlization(lifeEvents, cache, resource_handle{});
                 }
             }
         }
 
         template<class LifeEvents>
-            requires has_bind_event_v<LifeEvents> || has_use_event_v<LifeEvents>
-        void do_utilize(this const resourceful_context & self, const LifeEvents&, const resource_handle & h) {
+        [[nodiscard]]
+        auto& get_cache(this const resourceful_context& self, const LifeEvents&) {
+            static_assert(has_cache_v<LifeEvents>);
+
+            constexpr auto id{LifeEvents::caching_id};
+            using cache_t = index_cache<caching_identifier_constant<id>>;
+
+            return std::get<cache_t>(self.m_IndexCache);
+        }
+
+        template<class LifeEvents, caching_identifier id>
+            requires has_utilization_event_v<LifeEvents>
+        void cached_utlization(this const resourceful_context& self, const LifeEvents& lifeEvents, index_cache<caching_identifier_constant<id>>& cache, const resource_handle& h) {
+            self.do_utilize(lifeEvents, h);
+            cache.currently_active = h.index();
+        }
+
+        template<class LifeEvents>
+            requires has_utilization_event_v<LifeEvents>
+        void do_utilize(this const resourceful_context& self, const LifeEvents&, const resource_handle& h) {
             contextual_resource_view crv{self, h};
             if constexpr(has_bind_event_v<LifeEvents>) {
                 LifeEvents::bind(crv);
