25e187dd87003596aaae5d1a27897b6b28f39bcb
Oliver Rosten <oliver.rosten@gmail.com>
2026-05-19 14:09:11 +0100

    More code from ShaderProgramTrackingFreeTest.hpp/cpp to ResourceTrackingUtilities
    In addition to relocating the code, in preparation for live-coding, the following changes have been made:
    1. Renaming: any function names or member variables referencing 'shader_program' have had that part of the name swapped out for 'resource'
    2. Code which will not be templated during live-coding has been housed in resource_tracking_test_base
    3. The function formally known as check_program_data -- now resource_tracking_test_base::check_resource_data -- has an extra argument which carries the expected GPU call (in the present case, "UseProgram").

---
+++ Tests/OpenGL/Resources/ResourceTrackingUtilities.cpp
@@ -0,0 +1,224 @@
+////////////////////////////////////////////////////////////////////
+//                Copyright Oliver J. Rosten 2026.                //
+// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0. //
+//    (See accompanying file LICENSE.md or copy at                //
+//          https://www.gnu.org/licenses/gpl-3.0.en.html)         //
+////////////////////////////////////////////////////////////////////
+
+/*! \file */
+
+#include "ResourceTrackingUtilities.hpp"
+#include "OpenGL/ResourceInfrastructure/ResourceHandleTestingUtilities.hpp"
+
+#include "avocet/OpenGL/Context/GLFunction.hpp"
+
+#include "curlew/Window/GLFWWrappers.hpp"
+#include "curlew/Window/WindowConfigurations.hpp"
+
+#include <future>
+#include <thread>
+#include <latch>
+#include <optional>
+#include <ranges>
+
+namespace avocet::testing
+{
+    namespace agl = avocet::opengl;
+    namespace fs = std::filesystem;
+
+    namespace {
+        [[nodiscard]]
+        curlew::window_config make_window_config(std::vector<std::string>& calls) {
+            return curlew::make_call_logging_window_config({.width{1}, .height{1}}, calls, [](const agl::decorator_data& data) { return data.fn_name == "UseProgram"; });
+        }
+
+        [[nodiscard]]
+        agl::resource_handle get_current_resource_index(const agl::decorated_context& ctx) {
+            GLint param{};
+            agl::gl_function{&GladGLContext::GetIntegerv}(ctx, GL_CURRENT_PROGRAM, &param);
+            if(param < 0)
+                throw std::runtime_error{std::format("Negative resource index: {}", param)};
+
+            return agl::resource_handle{static_cast<GLuint>(param)};
+        }
+
+
+        using opt_latch_ref = std::optional<std::reference_wrapper<std::latch>>;
+
+        constexpr opt_latch_ref no_latch{};
+
+        using resource_handles = resource_tracking_test_base::resource_handles;
+
+        struct resource_artefacts {
+            agl::resource_handle handle;
+            std::optional<agl::shader_program> opt_shader_prog{};
+        };
+
+        enum class extend_resource_lifetime : bool { no, yes };
+
+        [[nodiscard]]
+        resource_artefacts make_and_use_resource(const curlew::window& w,
+            const fs::path& shaderDir,
+            opt_latch_ref entryLatch,
+            opt_latch_ref exitLatch,
+            extend_resource_lifetime extendResourceLifetime)
+        {
+            const auto& ctx{w.context()};
+
+            agl::shader_program sp{ctx, shaderDir / "Identity.vs", shaderDir / "Monochrome.fs"};
+            if(entryLatch) entryLatch->get().arrive_and_wait();
+
+            sp.use();
+            sp.use();
+
+            auto handle{get_current_resource_index(ctx)};
+
+            if(exitLatch) exitLatch->get().arrive_and_wait();
+
+            return {
+                std::move(handle),
+                extendResourceLifetime == extend_resource_lifetime::yes ? std::optional{std::move(sp)}
+                                                                        : std::optional<agl::shader_program>{}
+            };
+        }
+
+        [[nodiscard]]
+        resource_handles make_and_use_resource(curlew::window w, const fs::path& shaderDir) {
+            return {make_and_use_resource(w, shaderDir, no_latch, no_latch, extend_resource_lifetime::no).handle, get_current_resource_index(w.context())};
+        }
+
+        [[nodiscard]]
+        resource_artefacts make_and_use_extended_life_resource(const curlew::window& w, const fs::path& shaderDir) {
+            return make_and_use_resource(w, shaderDir, no_latch, no_latch, extend_resource_lifetime::yes);
+        }
+
+        using task_t = std::packaged_task<resource_handles()>;
+
+        [[nodiscard]]
+        task_t make_resource_task(curlew::window& w, const fs::path& shaderDir, opt_latch_ref entryLatch, opt_latch_ref exitLatch) {
+            curlew::test_window_manager::detach_current_context();
+
+            return task_t{
+                [&, entryLatch, exitLatch]() -> resource_handles {
+                    w.make_context_current();
+                    return {make_and_use_resource(w, shaderDir, entryLatch, exitLatch, extend_resource_lifetime::no).handle, get_current_resource_index(w.context())};
+                }
+            };
+        }
+
+        [[nodiscard]]
+        resource_handles make_and_use_resource_threaded(curlew::window w, const fs::path& shaderDir) {
+            auto task{make_resource_task(w, shaderDir, no_latch, no_latch)};
+
+            auto future{task.get_future()};
+
+            std::jthread worker{std::move(task)};
+
+            return future.get();
+        }
+
+        [[nodiscard]]
+        std::string make_description(std::string_view tag, std::string_view description)
+        {
+            return std::format("{}\n{}", tag, description);
+        }
+    }
+
+    void resource_tracking_test_base::check_resource_data(std::string_view tag, std::string_view expectedGPUCall, const gpu_data& data0, const gpu_data& data1)
+    {
+        check_resource_indices(tag, data0.resource, data1.resource);
+
+        auto buildPrediction{
+            [expectedGPUCall](const resource_handles& resource) {
+                std::vector<std::string> calls{std::string{expectedGPUCall}};
+                if(resource.after_destruction)
+                    calls.push_back(std::string{expectedGPUCall});
+
+                return calls;
+            }
+        };
+
+        check(equivalence, tag, data0.calls, buildPrediction(data0.resource));
+        check(equivalence, tag, data1.calls, buildPrediction(data1.resource));
+    }
+
+    void resource_tracking_test_base::check_reset_after_destruction(std::string_view tag, const resource_handles& resource)
+    {
+        if(resource.after_destruction)
+            check(equality, make_description(tag, "Resource should be reset to 0"), resource.after_destruction.value(), opengl::resource_handle{});
+    }
+
+    void resource_tracking_test_base::check_resource_indices(std::string_view tag, const resource_handles& resource0, const resource_handles& resource1)
+    {
+        check(make_description(tag, "Resource 0 should not be null"), resource0.active != opengl::resource_handle{});
+        check(make_description(tag, "Resource 1 should not be null"), resource1.active != opengl::resource_handle{});
+
+        check(equality, make_description(tag, "Assumption required for sensitivity to: Resource 0 utilization accidentally suppressing Resource 1 utilization"), resource0.active, resource1.active);
+
+        check_reset_after_destruction(tag, resource0);
+        check_reset_after_destruction(tag, resource1);
+    }
+
+    void resource_tracking_test::check_serial_tracking_non_overlapping_lifetimes()
+    {
+        gpu_data data0{}, data1{};
+
+        const auto shaderDir{working_materials()};
+        data0.resource = make_and_use_resource(create_window(make_window_config(data0.calls)), shaderDir);
+        data1.resource = make_and_use_resource(create_window(make_window_config(data1.calls)), shaderDir);
+
+        check_resource_data("Serial non-overlapping lifetimes", "UseProgram", data0, data1);
+    }
+
+    void resource_tracking_test::check_serial_tracking_overlapping_lifetimes()
+    {
+        const auto shaderDir{working_materials()};
+
+        gpu_data data0{};
+        auto win0{create_window(make_window_config(data0.calls))};
+        auto [handle0, sp0] {make_and_use_extended_life_resource(win0, shaderDir)};
+        data0.resource.active = std::move(handle0);
+
+        gpu_data data1{};
+        auto win1{create_window(make_window_config(data1.calls))};
+        auto [handle1, sp1] {make_and_use_extended_life_resource(win1, shaderDir)};
+        data1.resource.active = std::move(handle1);
+
+        check_resource_data("Serial overlapping lifetimes", "UseProgram", data0, data1);
+    }
+
+    void resource_tracking_test::check_threaded_tracking_non_overlapping_lifetimes()
+    {
+        gpu_data data0{}, data1{};
+
+        const auto shaderDir{working_materials()};
+        data0.resource = make_and_use_resource_threaded(create_window(make_window_config(data0.calls)), shaderDir);
+        data1.resource = make_and_use_resource_threaded(create_window(make_window_config(data1.calls)), shaderDir);
+
+        check_resource_data("Threaded non-overlapping lifetimes", "UseProgram", data0, data1);
+    }
+
+    void resource_tracking_test::check_threaded_tracking_overlapping_lifetimes()
+    {
+        const auto shaderDir{working_materials()};
+
+        std::array<gpu_data, 4> data{};
+
+        auto windows{to_array(std::span{data}, [this](gpu_data& d) { return create_window(make_window_config(d.calls)); })};
+
+        std::latch entryLatch{data.size()}, exitLatch{data.size()};
+        auto tasks{to_array(std::span{windows}, [&](curlew::window& win) { return make_resource_task(win, shaderDir, entryLatch, exitLatch); })};
+
+        auto futures{to_array(std::span{tasks}, [](task_t& t) { return t.get_future(); })};
+
+        auto workers{to_array(std::span{tasks}, [](task_t& t) { return std::jthread{std::move(t)}; })};
+
+        for(auto [gpuData, fut] : std::views::zip(data, futures)) {
+            gpuData.resource = fut.get();
+        }
+
+        for(auto i : std::views::iota(1uz, data.size()))
+            check_resource_data("Threaded overlapping lifetimes", "UseProgram", data[0], data[i]);
+    }
+
+}
