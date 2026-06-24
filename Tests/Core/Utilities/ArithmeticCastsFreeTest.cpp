aa2cebc76793da4c1ff53a0b9c31ad3a023c3c73
Oliver Rosten <oliver.rosten@gmail.com>
2026-06-24 16:13:26 +0100

    Imperfectly implement a function to convert between arithmetic types
    As discussed in the lecture, a fundamental - and difficult - choice to make for this function is whether or not it should have a wide/narrow contract. In our code base, it is always a bug for a conversion to be requested such that the input value is not in the range of the return value. This suggests that a narrow contract may be most appropriate. On the other hand, within he context of this project - which is for teaching - if there is a bug I want to reliably know about it as soon as possible, regardless of things like build configuration (which affect the behaviour of classic C-assert or C++26 contracts). Therefore, the decision was made to have a wide contract: the function is guaranteed to throw a std::domain_error in the case of non-sensical input. It is tacitly assumed that clients will not abuse this behaviour for dubious control flow.
    
    The other thing to note is that the implementation is distinctly sub-optimal. A clue comes from the GCC compiler, which warns about a comparison between signed and unsigned types. Understanding precisely what is going on will be done in a feature lecture, upon which a superior implementation will be provided.

--- Tests/Core/Utilities/ArithmeticCastsFreeTest.cpp
+++ Tests/Core/Utilities/ArithmeticCastsFreeTest.cpp
@@ -18,7 +18,62 @@ namespace avocet::testing
         return std::source_location::current().file_name();
     }
 
+    template<std::integral To, std::integral From>
+    inline constexpr bool has_lossless_conversion_v{
+        sequoia::initializable_from<To, From>
+    };
+
+    // If val is in the the range of the return type perform a
+    // static_cast; else throw a std::domain_error
+    template<std::integral To, std::integral From>
+    [[nodiscard]]
+    To checked_conversion_to(From val) noexcept(has_lossless_conversion_v<To, From>)
+    {
+        if constexpr(not has_lossless_conversion_v<To, From>) {
+            if(constexpr auto maxVal{std::numeric_limits<To>::max()}; val > maxVal)
+                throw std::domain_error{std::format("Value {} exceeds max value {} of target type", val, maxVal)};
+
+            if constexpr(std::is_signed_v<From>) {
+                if constexpr(std::is_signed_v<To>) {
+                    if(constexpr auto lowestVal{std::numeric_limits<To>::lowest()}; val < lowestVal)
+                        throw std::domain_error{std::format("Value {} lower than lowest value {} of target type", val, lowestVal)};
+                }
+                else if(val < 0)
+                    throw std::domain_error{std::format("Value {} lower than lowest value {} of target type", val, 0)};
+            }
+        }
+
+        return static_cast<To>(val);
+    }
+
     void arithmetic_casts_free_test::run_tests()
     {
+        check_exception_thrown<std::domain_error>(
+            "",
+            []() {
+                return checked_conversion_to<std::int32_t>(std::numeric_limits<std::int64_t>::max());
+            }
+        );
+
+        check_exception_thrown<std::domain_error>(
+            "",
+            []() {
+                return checked_conversion_to<std::int32_t>(std::numeric_limits<std::uint32_t>::max());
+            }
+        );
+
+        check_exception_thrown<std::domain_error>(
+            "",
+            []() {
+                return checked_conversion_to<std::uint32_t>(std::int32_t{-1});
+            }
+        );
+
+        check(equality, "", checked_conversion_to<int>(42), 42);
+
+        check(equality, "", checked_conversion_to<std:: int32_t>(std::numeric_limits<std::int64_t >::   max() >> 32), std::numeric_limits<std::int32_t>::max());
+        check(equality, "", checked_conversion_to<std:: int32_t>(std::numeric_limits<std::int64_t >::lowest() >> 32), std::numeric_limits<std::int32_t>::lowest());
+        check(equality, "", checked_conversion_to<std::uint32_t>(std::numeric_limits<std::uint64_t>::   max() >> 32), std::numeric_limits<std::uint32_t>::max());
+        check(equality, "", checked_conversion_to<std:: int32_t>(std::numeric_limits<std::uint32_t>::   max()  /  2), std::numeric_limits<std::int32_t>::max());
     }
 }
